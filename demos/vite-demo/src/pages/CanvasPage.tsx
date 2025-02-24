445ac0747f713c1da3d38dbfb7832b06d4110d7c
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2025-02-24 12:29:38 +0100

    demo

--- demos/vite-demo/src/pages/CanvasPage.tsx
+++ demos/vite-demo/src/pages/CanvasPage.tsx
@@ -1,66 +1,36 @@
-import { View, Text, useInputStreams, InputStream, Tiles } from '@swmansion/smelter';
+import { View, Text, useInputStreams, InputStream, Rescaler, Mp4 } from '@swmansion/smelter';
 import Smelter, { setWasmBundleUrl } from '@swmansion/smelter-web-wasm';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import SmelterCanvas from '../components/SmelterCanvas';
 
 setWasmBundleUrl('/assets/smelter.wasm');
 
-const CAMERA_ID = 'camera';
-const SCREEN_SHARE_ID = 'screen';
+const CLIP_ID = 'clip';
+
+const MP4_LONG = 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+const MP4_RACE = 'https://www.smelter.dev/videos/smelter-commercial.mp4';
 
 export default function CanvasPage() {
   const [smelter, setSmelter] = useState<Smelter>();
-  const [camera, setCamera] = useState<boolean>();
-  const [screen, setScreen] = useState<boolean>();
 
-  const toggleCamera = useCallback(async () => {
-    if (camera) {
-      try {
-        await smelter?.unregisterInput(CAMERA_ID)
-        setCamera(false);
-      } catch (err) {
-        console.log(err, 'Failed to unregister camera')
-      }
-    } else {
-      try {
-        await smelter?.registerInput(CAMERA_ID, { 'type': 'camera' });
-        setCamera(true);
-      } catch (err) {
-        console.log(err, 'Failed to register camera')
-      }
+  const addClip = useCallback(async () => {
+    await smelter?.unregisterInput(CLIP_ID).catch(() => { })
+    try {
+      await smelter?.registerInput(CLIP_ID, { 'type': 'mp4', 'url': MP4_RACE });
+    } catch (err) {
+      console.log(err, 'Failed to register camera')
     }
-  }, [smelter, camera])
-
-  const toggleScreenShare = useCallback(async () => {
-    if (screen) {
-      try {
-        await smelter?.unregisterInput(SCREEN_SHARE_ID)
-        setScreen(false);
-      } catch (err) {
-        console.log(err, 'Failed to unregister screen share input')
-      }
-    } else {
-      try {
-        await smelter?.registerInput(SCREEN_SHARE_ID, { 'type': 'screen_capture' });
-        setScreen(true);
-      } catch (err) {
-        console.log(err, 'Failed to register screen share input')
-      }
-    }
-  }, [smelter, screen])
+  }, [smelter])
 
 
   return (
     <div>
       <div style={{ textAlign: 'left' }}>
         <h3>Example canvas output</h3>
-        <button onClick={toggleCamera} style={{ margin: 10 }}>
-          Toggle camera
+        <button onClick={addClip} style={{ margin: 10 }}>
+          Run clip
         </button>
-        <button onClick={toggleScreenShare} style={{ margin: 10 }}>
-          Toggle screen capture
-        </button>
-        <p className="">Canvas: </p>
+        <p>Canvas: </p>
       </div>
       <div>
         <SmelterCanvas width={1280} height={720} onSmelterCreated={setSmelter}>
@@ -73,15 +43,37 @@ export default function CanvasPage() {
 
 function SmelterComponent() {
   const inputs = useInputStreams();
-  const hasCamera = !!inputs[CAMERA_ID];
-  const hasScreenShare = !!inputs[SCREEN_SHARE_ID];
+
+  const [lastClipEnd, setLastClipEnd] = useState<number | undefined>(undefined);
+  const [showClip, setShowClip] = useState<boolean>(false);
+
+  useEffect(() => {
+    const clip = inputs['clip'];
+    if ((lastClipEnd ?? 0) < Date.now() && clip?.videoState === 'playing') {
+      setLastClipEnd(Date.now() + (clip.videoDurationMs ?? 0))
+      setShowClip(true);
+      const durationMs = inputs['clip'].videoDurationMs;
+      if (durationMs) {
+        setTimeout(() => {
+          setShowClip(false)
+        }, durationMs - 500)
+      }
+    }
+  }, [inputs, lastClipEnd])
+
+
+  const mainPosition = showClip
+    ? { top: 20, left: 20, width: 320, height: 180, borderWidth: 10, borderRadius: 10 }
+    : { top: 0, left: 0, width: 1280, height: 720, borderWidth: 0, borderRadius: 0 };
+
   return (
     <View style={{ backgroundColor: '#302555' }}>
-      <Tiles>
-        {hasCamera ? <InputStream inputId={CAMERA_ID} /> : undefined}
-        {hasScreenShare ? <InputStream inputId={SCREEN_SHARE_ID} /> : undefined}
-        {!hasCamera && !hasScreenShare ? <Text style={{ fontSize: 100 }}>No input.{'\n'}Add camera and/or screen share.</Text> : undefined}
-      </Tiles>
+      <Rescaler>
+        <InputStream inputId='clip' />
+      </Rescaler>
+      <Rescaler style={{ ...mainPosition, borderColor: 'white' }} transition={{ durationMs: 500 }}>
+        <Mp4 source={MP4_LONG} />
+      </Rescaler>
       <View style={{ bottom: 0, left: 0, height: 50, padding: 20, backgroundColor: '#FFFFFF88' }}>
         <Text style={{ color: 'red', fontSize: 50 }}>Example app</Text>
       </View>
