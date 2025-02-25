ad8736bf6321d35d98cad9c6c1e8fe2ce16af86f
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2025-02-25 13:04:13 +0100

    Remove old docs

--- docs/pages/api/renderers/shader.md
+++
@@ -1,7 +0,0 @@
-import Docs from "@site/pages/api/generated/renderer-Shader.md"
-
-# Shader
-
-Represents compiled shader. Used by a [`Shader` component](../components/Shader).
-
-<Docs />
--- docs/src/monacoEditor.js
+++
@@ -1,4 +0,0 @@
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-export const languages = ExecutionEnvironment.canUseDOM ? require('monaco-editor').languages : null;
