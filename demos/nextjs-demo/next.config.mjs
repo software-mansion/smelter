15f77d132ab05957a7abf62525ad55592ff32195
Wojciech Kaźmierczak <wojciech.kazmierczak@swmansion.com>
2025-02-25 14:45:29 +0100

    integrated Smelter in the project

--- demos/nextjs-demo/next.config.mjs
+++ demos/nextjs-demo/next.config.mjs
@@ -1,4 +1,33 @@
+import path from 'node:path';
+import { createRequire } from 'node:module';
+import CopyPlugin from 'copy-webpack-plugin';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig =  {
+  webpack: (config, { isServer }) => {
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.join(
+              path.dirname(require.resolve('@swmansion/smelter-browser-render')),
+              'smelter.wasm'
+            ),
+            to: path.resolve('public'),
+          },
+        ],
+      })
+    );
+
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push('@swmansion/smelter-web-wasm');
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
