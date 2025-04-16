ae9e5804d0a0c8894baa7fb8202378353d70ca11
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2025-04-16 16:17:55 +0200

    benchmarks

---
+++ integration_tests/benchmarks/2025_04_28_9891af76/RESULTS.md
@@ -0,0 +1,235 @@
+## Benchmarks
+
+Following benchmarks were run on AWS using `benchmarks` executable from this crate.
+
+You can run them on your own with:
+```
+cargo run -r --bin benchmark -- --suite cpu  --json --json-file cpu_benchmark.json
+cargo run -r --bin benchmark -- --suite full --json --json-file full_benchmark.json
+```
+
+Following benchmarks compare different EC2 instances (both CPU only and CPU+GPU).
+
+Each of the following examples is testing max number of inputs and outputs that the instance can handle.
+It provides 3 variants with different ratio of inputs to outputs. Each output renders 1, 2 or 4 inputs 
+using tiles.
+
+### `c5.xlarge` 4vCPU 8GB
+
+- Input: 480p24fps
+- Output: 480p24fps
+
+| Input/output ratio | Encoder preset | Max inputs | Max outputs |
+| ------------------ | -------------- | ---------- | ----------- |
+| 1:1                | ultrafast      | **2**      | **2**       |
+| 1:1                | veryfast       | **2**      | **2**       |
+| 1:1                | fast           | **1**      | **1**       |
+| 2:1                | ultrafast      | **2**      | **1**       |
+| 2:1                | veryfast       | **2**      | **1**       |
+| 2:1                | fast           | **2**      | **1**       |
+| 4:1                | ultrafast      | **4**      | **1**       |
+| 4:1                | veryfast       | **4**      | **1**       |
+| 4:1                | fast           | **4**      | **1**       |
+
+---
+
+- Input: 480p24fps
+- Output: 720p24fps
+
+| Input/output ratio | Encoder preset | Max inputs | Max outputs |
+| ------------------ | -------------- | ---------- | ----------- |
+| 1:1                | ultrafast      | **1**      | **1**       |
+| 1:1                | veryfast       | **1**      | **1**       |
+| 1:1                | fast           | **1**      | **1**       |
+| 2:1                | ultrafast      | **2**      | **1**       |
+| 2:1                | veryfast       | **2**      | **1**       |
+| 2:1                | fast           | **2**      | **1**       |
+| 4:1                | ultrafast      | **-**      | **-**       |
+| 4:1                | veryfast       | **-**      | **-**       |
+| 4:1                | fast           | **-**      | **-**       |
+
+---
+
+- Input: 720p24fps
+- Output: 720p24fps
+
+It can only handle one input and one output with ultrafast preset
+
+---
+
+### `c5.2xlarge` 8vCPU 16GB
+
+- Input: 480p24fps
+- Output: 480p24fps
+
+| Input/output ratio | Encoder preset | Max inputs | Max outputs |
+| ------------------ | ----------     | ---------- | ----------- |
+| 1:1                | ultrafast      | **4**      | **4**       |
+| 1:1                | veryfast       | **3**      | **3**       |
+| 1:1                | fast           | **3**      | **3**       |
+| 2:1                | ultrafast      | **6**      | **3**       |
+| 2:1                | veryfast       | **6**      | **3**       |
+| 2:1                | fast           | **6**      | **3**       |
+| 4:1                | ultrafast      | **8**      | **2**       |
+| 4:1                | veryfast       | **8**      | **2**       |
+| 4:1                | fast           | **4**      | **1**       |
+
+---
+
+- Input: 720p24fps
+- Output: 720p24fps
+
+| Input/output ratio | Encoder preset | Max inputs | Max outputs |
+| ------------------ | -------------- | ---------  | ----------- |
+| 1:1                | ultrafast      | **1**      | **1**       |
+| 1:1                | veryfast       | **1**      | **1**       |
+| 1:1                | fast           | **1**      | **1**       |
+| 2:1                | ultrafast      | **2**      | **1**       |
+| 2:1                | veryfast       | **2**      | **1**       |
+| 2:1                | fast           | **2**      | **1**       |
+| 4:1                | ultrafast      | **4**      | **1**       |
+| 4:1                | veryfast       | **-**      | **-**       |
+| 4:1                | fast           | **-**      | **-**       |
+
+---
+
+### `c5.4xlarge` 16vCPU 32GB
+
+- Input: 480p24fps
+- Output: 480p24fps
+
+| Input/output ratio | Encoder preset | Max inputs | Max outputs |
+| ------------------ | -------------- | ---------- | ----------- |
+| 1:1                | ultrafast      | **6**      | **6**       |
+| 1:1                | veryfast       | **6**      | **6**       |
+| 1:1                | fast           | **5**      | **5**       |
+| 2:1                | ultrafast      | **10**     | **5**       |
+| 2:1                | veryfast       | **10**     | **5**       |
+| 2:1                | fast           | **10**     | **5**       |
+| 4:1                | ultrafast      | **8**      | **2**       |
+| 4:1                | veryfast       | **8**      | **2**       |
+| 4:1                | fast           | **4**      | **1**       |
+
+---
+
+- Input: 720p24fps
+- Output: 720p24fps
+
+| Input/output ratio | Encoder preset | Max inputs | Max outputs |
+| ------------------ | -------------- | ---------- | ----------- |
+| 1:1                | ultrafast      | **3**      | **3**       |
+| 1:1                | veryfast       | **3**      | **3**       |
+| 1:1                | fast           | **2**      | **2**       |
+| 2:1                | ultrafast      | **6**      | **3**       |
+| 2:1                | veryfast       | **4**      | **2**       |
+| 2:1                | fast           | **4**      | **2**       |
+| 4:1                | ultrafast      | **4**      | **1**       |
+| 4:1                | veryfast       | **4**      | **1**       |
+| 4:1                | fast           | **4**      | **1**       |
+
+---
+
+### `g4dn.xlarge` 4vCPU 16GB + GPU Nvidia T4
+
+- Input: 480p24fps
+- Output: 480p24fps
+
+| Input/output ratio | Encoder preset | Max inputs | Max outputs |
+| ------------------ | -------------- | ---------- | ----------- |
+| 1:1                | ultrafast      | **24**     | **24**      |
+| 1:1                | veryfast       | **13**     | **13**      |
+| 1:1                | fast           | **6**      | **6**       |
+| 2:1                | ultrafast      | **46**     | **23**      |
+| 2:1                | veryfast       | **28**     | **14**      |
+| 2:1                | fast           | **16**     | **8**       |
+| 4:1                | ultrafast      | **64**     | **16**      |
+| 4:1                | veryfast       | **40**     | **10**      |
+| 4:1                | fast           | **20**     | **5**       |
+
+---
+
+- Input: 720p24fps
+- Output: 720p24fps
+
+| Input/output ratio | Encoder preset | Max inputs | Max outputs |
+| ------------------ | -------------- | ---------- | ----------- |
+| 1:1                | ultrafast      | **12**     | **12**      |
+| 1:1                | veryfast       | **6**      | **6**       |
+| 1:1                | fast           | **3**      | **3**       |
+| 2:1                | ultrafast      | **26**     | **13**      |
+| 2:1                | veryfast       | **14**     | **7**       |
+| 2:1                | fast           | **6**      | **3**       |
+| 4:1                | ultrafast      | **32**     | **8**       |
+| 4:1                | veryfast       | **20**     | **5**       |
+| 4:1                | fast           | **8**      | **2**       |
+
+---
+
+- Output: 1080p 30fps
+- Input: 1080p 30fps
+
+| Input/output ratio | Encoder preset | Max inputs | Max outputs |
+| ------------------ | -------------- | ---------- | ----------- |
+| 1:1                | ultrafast      | **5**      | **5**       |
+| 1:1                | veryfast       | **2**      | **2**       |
+| 1:1                | fast           | **1**      | **1**       |
+| 2:1                | ultrafast      | **10**     | **5**       |
+| 2:1                | veryfast       | **6**      | **3**       |
+| 2:1                | fast           | **2**      | **1**       |
+| 4:1                | ultrafast      | **16**     | **4**       |
+| 4:1                | veryfast       | **8**      | **2**       |
+| 4:1                | fast           | **-**      | **-**       |
+
+---
+
+### `c4dn.2xlarge` 16vCPU 32GB + GPU Nvidia T4
+
+- Input: 480p24fps
+- Output: 480p24fps
+
+| Input/output ratio | Encoder preset | Max inputs | Max outputs |
+| ------------------ | -------------- | ---------- | ----------- |
+| 1:1                | ultrafast      | **39**     | **39**      |
+| 1:1                | veryfast       | **23**     | **23**      |
+| 1:1                | fast           | **12**     | **12**      |
+| 2:1                | ultrafast      | **72**     | **36**      |
+| 2:1                | veryfast       | **48**     | **24**      |
+| 2:1                | fast           | **30**     | **15**      |
+| 4:1                | ultrafast      | **80**     | **20**      |
+| 4:1                | veryfast       | **64**     | **16**      |
+| 4:1                | fast           | **40**     | **10**      |
+
+---
+
+- Input: 720p24fps
+- Output: 720p24fps
+
+| Input/output ratio | Encoder preset | Max inputs | Max outputs |
+| ------------------ | -------------- | ---------- | ----------- |
+| 1:1                | ultrafast      | **22**     | **22**      |
+| 1:1                | veryfast       | **12**     | **12**      |
+| 1:1                | fast           | **6**      | **6**       |
+| 2:1                | ultrafast      | **36**     | **18**      |
+| 2:1                | veryfast       | **28**     | **14**      |
+| 2:1                | fast           | **14**     | **7**       |
+| 4:1                | ultrafast      | **36**     | **9**       |
+| 4:1                | veryfast       | **36**     | **9**       |
+| 4:1                | fast           | **16**     | **4**       |
+
+---
+
+- Output: 1080p 30fps
+- Input: 1080p 30fps
+
+| Input/output ratio | Encoder preset | Max inputs | Max outputs |
+| ------------------ | -------------- | ---------- | ----------- |
+| 1:1                | ultrafast      | **10**     | **10**      |
+| 1:1                | veryfast       | **4**      | **4**       |
+| 1:1                | fast           | **2**      | **2**       |
+| 2:1                | ultrafast      | **18**     | **9**       |
+| 2:1                | veryfast       | **12**     | **6**       |
+| 2:1                | fast           | **6**      | **3**       |
+| 4:1                | ultrafast      | **20**     | **5**       |
+| 4:1                | veryfast       | **16**     | **4**       |
+| 4:1                | fast           | **4**      | **1**       |
+
--- integration_tests/src/bin/benchmark/benchmark.rs
+++ integration_tests/src/bin/benchmark/benchmark.rs
@@ -1,46 +1,13 @@
-use std::{path::PathBuf, time::Duration};
+use std::{sync::Arc, time::Duration};
 
-use compositor_pipeline::pipeline::{
-    self,
-    encoder::ffmpeg_h264::{self, EncoderPreset},
-    GraphicsContext, VideoDecoder,
-};
-use compositor_render::{scene::Component, OutputId};
-use serde_json::json;
+use compositor_pipeline::pipeline::{encoder::ffmpeg_h264, GraphicsContext};
+use serde_json::{json, Value as JsonValue};
 use tracing::{error, info};
 
 use crate::{
-    args::{NumericArgument, Resolution, ResolutionArgument},
-    benchmark_pass::{InputFile, SingleBenchmarkPass},
-    maximize_iter::{Const, MaximizeIter, MaximizeResolution, MaximizeU64},
-    scenes::{simple_tiles_with_all_inputs, SceneContext},
+    benchmark_pass::SingleBenchmarkPass,
+    maximize_iter::{MaximizeIter, MaximizeU64},
 };
-
-#[derive(Debug, Clone, Copy)]
-pub enum ValueOrMaximized<T: Clone + Copy> {
-    Value(T),
-    Maximize,
-}
-
-impl From<NumericArgument> for ValueOrMaximized<u64> {
-    fn from(value: NumericArgument) -> Self {
-        match value {
-            NumericArgument::IterateExp => panic!(),
-            NumericArgument::Maximize => ValueOrMaximized::Maximize,
-            NumericArgument::Constant(v) => ValueOrMaximized::Value(v),
-        }
-    }
-}
-
-impl From<ResolutionArgument> for ValueOrMaximized<Resolution> {
-    fn from(value: ResolutionArgument) -> Self {
-        match value {
-            ResolutionArgument::Iterate => panic!(),
-            ResolutionArgument::Maximize => ValueOrMaximized::Maximize,
-            ResolutionArgument::Constant(v) => ValueOrMaximized::Value(v),
-        }
-    }
-}
 
 #[derive(Debug, Clone, Copy)]
 pub enum EncoderOptions {
@@ -48,59 +15,31 @@ pub enum EncoderOptions {
     Disabled,
 }
 
-#[derive(Debug, Clone)]
+type BenchPassBuilder = Box<dyn (Fn(u64) -> SingleBenchmarkPass)>;
+
+#[derive(Clone)]
 pub struct Benchmark {
     pub id: &'static str,
-    pub scene_builder: fn(ctx: &SceneContext, output_id: &OutputId) -> Component,
-
-    pub input_count: ValueOrMaximized<u64>,
-    pub output_count: ValueOrMaximized<u64>,
-    pub framerate: ValueOrMaximized<u64>,
-    pub output_resolution: ValueOrMaximized<Resolution>,
-
-    pub input_file: InputFile,
-    pub encoder: EncoderOptions,
-    pub decoder: pipeline::VideoDecoder,
-
-    pub warm_up_time: Duration,
-    pub measure_time: Duration,
-
-    pub error_tolerance_multiplier: f64,
+    pub bench_pass_builder: Arc<BenchPassBuilder>,
 }
 
-#[derive(Debug)]
 pub struct BenchmarkResult {
+    pub id: &'static str,
     pub pass: Option<SingleBenchmarkPass>,
-    pub config: Benchmark,
 }
-
-type Maximizer<T> = Box<dyn MaximizeIter<T>>;
 
 impl Benchmark {
     pub fn run(&self, ctx: &GraphicsContext) -> BenchmarkResult {
-        let (mut input_count, mut output_count, mut framerate, mut output_resolution) =
-            self.maximizers();
+        let mut maximizer = MaximizeU64::new(1);
 
         let mut last_valid_pass = None;
         let mut prev_success = true;
-        while let (
-            Some(input_count),
-            Some(output_count),
-            Some(framerate),
-            Some(output_resolution),
-        ) = (
-            input_count.next(prev_success),
-            output_count.next(prev_success),
-            framerate.next(prev_success),
-            output_resolution.next(prev_success),
-        ) {
-            let pass = SingleBenchmarkPass {
-                input_count,
-                output_count,
-                framerate,
-                output_resolution,
-                ..self.default_single_bench()
-            };
+        while let Some(current) = maximizer.next(prev_success) {
+            let mut pass = (*self.bench_pass_builder)(current);
+
+            // add first 30 frames to warm_up_time
+            pass.warm_up_time += Duration::from_secs_f64(30.0 / pass.framerate as f64);
+
             prev_success = match pass.run(ctx.clone()) {
                 Ok(is_success) => is_success,
                 Err(err) => {
@@ -112,184 +51,40 @@ impl Benchmark {
                 true => "PASS",
                 false => "FAIL",
             };
-            info!(
-                "{}: (input: {}, output: {}, framerate: {}, resolution: {:?})",
-                status_str, input_count, output_count, framerate, output_resolution
-            );
+            info!("{}: ({:?})", status_str, pass);
             if prev_success {
                 last_valid_pass = Some(pass);
             }
         }
 
         BenchmarkResult {
+            id: self.id,
             pass: last_valid_pass,
-            config: self.clone(),
-        }
-    }
-
-    fn maximizers(
-        &self,
-    ) -> (
-        Maximizer<u64>,
-        Maximizer<u64>,
-        Maximizer<u64>,
-        Maximizer<Resolution>,
-    ) {
-        match (
-            self.input_count,
-            self.output_count,
-            self.framerate,
-            self.output_resolution,
-        ) {
-            (
-                ValueOrMaximized::Maximize,
-                ValueOrMaximized::Value(output_count),
-                ValueOrMaximized::Value(framerate),
-                ValueOrMaximized::Value(output_resolution),
-            ) => (
-                Box::from(MaximizeU64::new(0)),
-                Box::from(Const(output_count)),
-                Box::from(Const(framerate)),
-                Box::from(Const(output_resolution)),
-            ),
-            (
-                ValueOrMaximized::Value(input_count),
-                ValueOrMaximized::Maximize,
-                ValueOrMaximized::Value(framerate),
-                ValueOrMaximized::Value(output_resolution),
-            ) => (
-                Box::from(Const(input_count)),
-                Box::from(MaximizeU64::new(0)),
-                Box::from(Const(framerate)),
-                Box::from(Const(output_resolution)),
-            ),
-            (
-                ValueOrMaximized::Value(input_count),
-                ValueOrMaximized::Value(output_count),
-                ValueOrMaximized::Maximize,
-                ValueOrMaximized::Value(output_resolution),
-            ) => (
-                Box::from(Const(input_count)),
-                Box::from(Const(output_count)),
-                Box::from(MaximizeU64::new(1)),
-                Box::from(Const(output_resolution)),
-            ),
-            (
-                ValueOrMaximized::Value(input_count),
-                ValueOrMaximized::Value(output_count),
-                ValueOrMaximized::Value(framerate),
-                ValueOrMaximized::Maximize,
-            ) => (
-                Box::from(Const(input_count)),
-                Box::from(Const(output_count)),
-                Box::from(Const(framerate)),
-                Box::from(MaximizeResolution::new()),
-            ),
-            _ => panic!("only one option can be set to maximized"),
-        }
-    }
-
-    fn default_single_bench(&self) -> SingleBenchmarkPass {
-        SingleBenchmarkPass {
-            scene_builder: self.scene_builder,
-
-            input_count: 0,
-            output_count: 0,
-            framerate: 0,
-            output_resolution: Resolution {
-                width: 0,
-                height: 0,
-            },
-
-            input_file: self.input_file.clone(),
-
-            encoder: self.encoder,
-            decoder: self.decoder,
-
-            warm_up_time: self.warm_up_time,
-            measure_time: self.measure_time,
-
-            error_tolerance_multiplier: self.error_tolerance_multiplier,
-        }
-    }
-}
-
-impl Default for Benchmark {
-    fn default() -> Self {
-        Self {
-            id: "",
-            scene_builder: simple_tiles_with_all_inputs,
-
-            input_count: ValueOrMaximized::Value(0),
-            output_count: ValueOrMaximized::Value(0),
-            framerate: ValueOrMaximized::Value(0),
-            output_resolution: ValueOrMaximized::Value(Resolution {
-                width: 1,
-                height: 1,
-            }),
-
-            input_file: InputFile::Mp4(PathBuf::new()), // always override
-
-            encoder: EncoderOptions::Enabled(EncoderPreset::Ultrafast),
-            decoder: VideoDecoder::FFmpegH264,
-
-            warm_up_time: Duration::from_secs(10),
-            measure_time: Duration::from_secs(20),
-
-            error_tolerance_multiplier: 1.10,
         }
     }
 }
 
 impl BenchmarkResult {
-    pub fn json(&self) -> serde_json::Value {
-        let config = &self.config;
-        let (key_result, value_result) = self.get_maximized();
+    pub fn json(&self) -> JsonValue {
         json!({
-          "id": self.config.id,
-          "result": {
-            key_result: value_result
-          },
-          "config": {
-            "input_count": format!("{:?}", config.input_count),
-            "output_count": format!("{:?}", config.output_count),
-            "framerate": format!("{:?}", config.framerate),
-            "output_resolution": format!("{:?}", config.output_resolution),
-
-            "input_file": format!("{:?}", config.input_file),
-            "encoder": format!("{:?}", config.encoder),
-            "decoder": format!("{:?}", config.decoder),
-
-            "warm_up_time": config.warm_up_time,
-            "measured_time": config.measure_time,
-            "error_tolerance_multiplier": config.error_tolerance_multiplier,
+          "id": self.id,
+          "config": match &self.pass {
+            Some(pass) => json!({
+              "input_count": format!("{:?}", pass.input_count),
+              "output_count": format!("{:?}", pass.output_count),
+              "framerate": format!("{:?}", pass.framerate),
+              "output_resolution": format!("{:?}", pass.output_resolution),
+              "input_file": format!("{:?}", pass.input_file),
+              "encoder": format!("{:?}", pass.encoder),
+              "decoder": format!("{:?}", pass.decoder),
+              "warm_up_time": pass.warm_up_time,
+            }),
+            None => serde_json::Value::Null,
           }
         })
     }
 
     pub fn text(&self) -> String {
-        format!(
-            "id: {:?}\nresult:{:?}\n",
-            &self.config.id,
-            self.get_maximized()
-        )
-    }
-
-    fn get_maximized(&self) -> (&str, String) {
-        if let ValueOrMaximized::Maximize = self.config.input_count {
-            let value = self.pass.as_ref().map(|p| p.input_count);
-            ("input_count", format!("{:?}", value))
-        } else if let ValueOrMaximized::Maximize = self.config.output_count {
-            let value = self.pass.as_ref().map(|p| p.output_count);
-            ("output_count", format!("{:?}", value))
-        } else if let ValueOrMaximized::Maximize = self.config.framerate {
-            let value = self.pass.as_ref().map(|p| p.framerate);
-            ("framerate", format!("{:?}", value))
-        } else if let ValueOrMaximized::Maximize = self.config.output_resolution {
-            let value = self.pass.as_ref().map(|p| p.output_resolution);
-            ("output_resolution", format!("{:?}", value))
-        } else {
-            ("no-maximize", "true".to_string())
-        }
+        format!("id: {:?}\nresult: {:?}\n", &self.id, &self.pass)
     }
 }
