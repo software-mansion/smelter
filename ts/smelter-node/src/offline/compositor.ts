d2dd091cd15093ee953e84b15fa31f59bc388db8
Wojciech Kaźmierczak <wojciech.kazmierczak@swmansion.com>
2025-04-28 14:10:49 +0200

    bearer token as result of register input

--- ts/smelter-node/src/offline/compositor.ts
+++ ts/smelter-node/src/offline/compositor.ts
@@ -29,8 +29,20 @@ export default class OfflineSmelter {
     await this.coreSmelter.render(root, request, durationMs);
   }
 
-  public async registerInput(inputId: string, request: CoreInput.RegisterInput) {
-    await this.coreSmelter.registerInput(inputId, request);
+  public async registerInput(inputId: string, request: CoreInput.RegisterInput): Promise<object> {
+    let result = await this.coreSmelter.registerInput(inputId, request);
+    const mappedResult: any = {};
+
+    if ('bearer_token' in result) {
+      mappedResult.bearerToken = result['bearer_token'];
+    }
+    if ('video_duration_ms' in result) {
+      mappedResult.videoDurationMs = result['video_duration_ms'];
+    }
+    if ('audio_duration_ms' in result) {
+      mappedResult.audioDurationMs = result['audio_duration_ms'];
+    }
+    return mappedResult;
   }
 
   public async registerImage(imageId: string, request: Renderers.RegisterImage): Promise<void> {
