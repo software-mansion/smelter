0bd9b9b8de6d2b50a1ec6d69ec83de2ea6fab1e7
noituri <mikolaj.radkowski@protonmail.com>
2025-04-29 15:52:03 +0200

    [ts-sdk] Bump smelter binary version

--- ts/smelter-node/src/manager/locallySpawnedInstance.ts
+++ ts/smelter-node/src/manager/locallySpawnedInstance.ts
@@ -18,7 +18,7 @@ import { killProcess, spawn } from '../spawn';
 import { WebSocketConnection } from '../ws';
 import { smelterInstanceLoggerOptions } from '../logger';
 
-const VERSION = `v0.4.1`;
+const VERSION = `v0.4.2`;
 
 type ManagedInstanceOptions = {
   port: number;
