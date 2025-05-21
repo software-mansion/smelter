990ee94c73084b8098ae9545eeeba2f5f3772b07
noituri <mikolaj.radkowski@protonmail.com>
2025-05-21 13:43:34 +0200

    [wasm] Update wgpu to 25.0.0

--- compositor_web/src/wasm/wgpu.rs
+++ compositor_web/src/wasm/wgpu.rs
@@ -7,6 +7,16 @@ use super::types::to_js_error;
 pub async fn create_wgpu_context() -> Result<(Arc<wgpu::Device>, Arc<wgpu::Queue>), JsValue> {
     let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
         backends: wgpu::Backends::GL,
+        backend_options: wgpu::BackendOptions {
+            gl: wgpu::GlBackendOptions {
+                // Default behavior for fences on WebGL breaks `device.poll(Wait)` (always timeouts).
+                // `AutoFinish` makes the web behavior consistent with native at the cost of `Queue::on_completed_work_done` not working correctly.
+                // https://github.com/gfx-rs/wgpu/blob/a95c69eb910c78306c4f19212183177f51f99aea/wgpu-types/src/instance.rs#L548-L560
+                fence_behavior: wgpu::GlFenceBehavior::AutoFinish,
+                ..Default::default()
+            },
+            ..Default::default()
+        },
         ..Default::default()
     });
 
@@ -23,21 +33,19 @@ pub async fn create_wgpu_context() -> Result<(Arc<wgpu::Device>, Arc<wgpu::Queue
             compatible_surface: Some(&surface),
         })
         .await
-        .ok_or(JsValue::from_str("Failed to get a wgpu adapter"))?;
+        .map_err(|_| JsValue::from_str("Failed to get a wgpu adapter"))?;
     let (device, queue) = adapter
-        .request_device(
-            &wgpu::DeviceDescriptor {
-                label: None,
-                required_features: wgpu::Features::PUSH_CONSTANTS,
-                required_limits: wgpu::Limits {
-                    max_push_constant_size: 128,
-                    max_color_attachments: 6,
-                    ..wgpu::Limits::downlevel_webgl2_defaults()
-                },
-                memory_hints: wgpu::MemoryHints::default(),
+        .request_device(&wgpu::DeviceDescriptor {
+            label: None,
+            required_features: wgpu::Features::PUSH_CONSTANTS,
+            required_limits: wgpu::Limits {
+                max_push_constant_size: 128,
+                max_color_attachments: 6,
+                ..wgpu::Limits::downlevel_webgl2_defaults()
             },
-            None,
-        )
+            memory_hints: wgpu::MemoryHints::default(),
+            trace: wgpu::Trace::Off,
+        })
         .await
         .map_err(to_js_error)?;
 
