bb168b13faef399f44a16e565f5c438e02d836fd
noituri <mikolaj.radkowski@protonmail.com>
2025-06-12 17:18:46 +0200

    [ts-sdk] Synchronize calls to smelter

--- ts/smelter-core/src/utils.ts
+++ ts/smelter-core/src/utils.ts
@@ -61,3 +61,79 @@ export async function sleep(timeoutMs: number): Promise<void> {
     }, timeoutMs);
   });
 }
+
+export class StateGuard {
+  private state:
+    | {
+        type: 'blocked';
+        blocking_promise: Promise<void>;
+        nonblocking_promises?: Set<Promise<void>>;
+      }
+    | { type: 'running'; promises: Set<Promise<void>> }
+    | { type: 'finished' };
+
+  public constructor() {
+    this.state = { type: 'finished' };
+  }
+
+  public async runBlocking<T>(fn: () => Promise<T>): Promise<T> {
+    const [blocking_promise, release] = this.createGuardPromise();
+    while (this.state.type !== 'finished') {
+      if (this.state.type === 'blocked') {
+        await Promise.allSettled(this.state?.nonblocking_promises ?? []);
+        await this.state.blocking_promise;
+      } else if (this.state.type === 'running') {
+        this.state = {
+          type: 'blocked',
+          blocking_promise,
+          nonblocking_promises: this.state.promises,
+        };
+        await Promise.allSettled(this.state?.nonblocking_promises ?? []);
+      }
+    }
+
+    try {
+      this.state = { type: 'blocked', blocking_promise };
+      return await fn();
+    } finally {
+      this.state = { type: 'finished' };
+      release();
+    }
+  }
+
+  public async run<T>(fn: () => Promise<T>): Promise<T> {
+    while (this.state.type === 'blocked') {
+      await this.state.blocking_promise;
+    }
+
+    const [promise, release] = this.createGuardPromise();
+
+    try {
+      if (this.state.type === 'running') {
+        this.state.promises.add(promise);
+      } else if (this.state.type === 'finished') {
+        this.state = { type: 'running', promises: new Set([promise]) };
+      }
+
+      return await fn();
+    } finally {
+      if (this.state.type === 'running') {
+        this.state.promises.delete(promise);
+        if (this.state.promises.size === 0) {
+          this.state = { type: 'finished' };
+        }
+      }
+
+      release();
+    }
+  }
+
+  private createGuardPromise(): [Promise<void>, () => void] {
+    let release: (() => void) | undefined;
+    const promise = new Promise<void>(res => {
+      release = res;
+    });
+
+    return [promise, release as () => void];
+  }
+}
--- ts/smelter-node/src/offline/compositor.ts
+++ ts/smelter-node/src/offline/compositor.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import FormData from 'form-data';
 import type { ReactElement } from 'react';
 import type { SmelterManager } from '@swmansion/smelter-core';
-import { OfflineSmelter as CoreSmelter } from '@swmansion/smelter-core';
+import { OfflineSmelter as CoreSmelter, StateGuard } from '@swmansion/smelter-core';
 import type { Renderers } from '@swmansion/smelter';
 
 import type { RegisterInput, RegisterOutput } from '../api';
@@ -11,32 +11,44 @@ import LocallySpawnedInstanceManager from '../manager/locallySpawnedInstance';
 
 export default class OfflineSmelter {
   private coreSmelter: CoreSmelter;
+  private scheduler: StateGuard;
 
   public constructor(manager?: SmelterManager) {
     this.coreSmelter = new CoreSmelter(
       manager ?? LocallySpawnedInstanceManager.defaultManager(),
       createLogger()
     );
+    this.scheduler = new StateGuard();
   }
 
   public async init(): Promise<void> {
-    await this.coreSmelter.init();
+    await this.scheduler.runBlocking(async () => {
+      await this.coreSmelter.init();
+    });
   }
 
   public async render(root: ReactElement, request: RegisterOutput, durationMs?: number) {
-    await this.coreSmelter.render(root, request, durationMs);
+    await this.scheduler.runBlocking(async () => {
+      await this.coreSmelter.render(root, request, durationMs);
+    });
   }
 
   public async registerInput(inputId: string, request: RegisterInput) {
-    await this.coreSmelter.registerInput(inputId, request);
+    await this.scheduler.run(async () => {
+      await this.coreSmelter.registerInput(inputId, request);
+    });
   }
 
   public async registerImage(imageId: string, request: Renderers.RegisterImage): Promise<void> {
-    await this.coreSmelter.registerImage(imageId, request);
+    await this.scheduler.run(async () => {
+      await this.coreSmelter.registerImage(imageId, request);
+    });
   }
 
   public async registerShader(shaderId: string, request: Renderers.RegisterShader): Promise<void> {
-    await this.coreSmelter.registerShader(shaderId, request);
+    await this.scheduler.run(async () => {
+      await this.coreSmelter.registerShader(shaderId, request);
+    });
   }
 
   public async registerFont(fontSource: string | ArrayBuffer): Promise<object> {
@@ -55,10 +67,12 @@ export default class OfflineSmelter {
     const formData = new FormData();
     formData.append('fontFile', fontBuffer);
 
-    return this.coreSmelter.manager.sendMultipartRequest({
-      method: 'POST',
-      route: `/api/font/register`,
-      body: formData,
+    return await this.scheduler.run(async () => {
+      return this.coreSmelter.manager.sendMultipartRequest({
+        method: 'POST',
+        route: `/api/font/register`,
+        body: formData,
+      });
     });
   }
 }
