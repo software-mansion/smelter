375dc50fcdc46328642575104fb6dc19fa7bc926
Jakub Rękas <jakub.rekas@swmansion.com>
2025-07-16 13:21:58 +0200

    Added new sum scale behaviour

--- compositor_pipeline/src/audio_mixer/mix.rs
+++ compositor_pipeline/src/audio_mixer/mix.rs
@@ -9,6 +9,11 @@ use super::{
     OutputInfo,
 };
 
+use tracing::trace;
+
+const SCALING_THRESHOLD: f64 = 0.95f64 * i64::MAX as f64;
+const SCALING_INCREMENT: f64 = 0.01f64;
+
 // I don't know if this is a good name, correct me if I'm wrong
 #[derive(Debug)]
 pub(super) struct SampleMixer {
@@ -21,9 +26,10 @@ impl SampleMixer {
             scaling_factor: 1.0f64,
         }
     }
+
     /// Mix input samples accordingly to provided specification.
     pub fn mix_samples(
-        &self,
+        &mut self,
         input_samples: &HashMap<InputId, Vec<(i16, i16)>>,
         output_info: &OutputInfo,
         samples_count: usize,
@@ -58,26 +64,51 @@ impl SampleMixer {
             .collect()
     }
 
-    fn sum_scale(&self, summed_samples: Vec<(i64, i64)>) -> Vec<(i16, i16)> {
-        let scaling_factor = {
-            // abs panics in debug if val = i64::MIN, but it would require summing so many i16 samples, that it'll never happen.
-            // Assumes that summed samples is not empty (therefore unwrap is safe)
-            let max_abs = summed_samples
-                .iter()
-                .map(|(l, r)| i64::max(l.abs(), r.abs()))
-                .max()
-                .unwrap();
-            f64::max(max_abs as f64 / i16::MAX as f64, 1.0)
+    fn sum_scale(&mut self, summed_samples: Vec<(i64, i64)>) -> Vec<(i16, i16)> {
+        let summed_samples: Vec<(f64, f64)> = summed_samples
+            .into_iter()
+            .map(|(l, r)| (l as f64, r as f64))
+            .collect();
+
+        // Assumes that summed samples is not empty (therefore unwrap is safe)
+        let max_sample = summed_samples
+            .iter()
+            .map(|(l, r)| f64::max(l.abs(), r.abs()))
+            .reduce(f64::max)
+            .expect("Assumes that summed samples is not empty");
+        trace!("Max abs value: {max_sample}");
+
+        let new_scaling_factor = if max_sample > SCALING_THRESHOLD {
+            self.scaling_factor - SCALING_INCREMENT
+        } else {
+            self.scaling_factor
         };
+        trace!("Old scaling factor: {}", self.scaling_factor);
+        trace!("New scaling factor: {new_scaling_factor}");
+
+        let interpolation_increment =
+            (self.scaling_factor - new_scaling_factor) / summed_samples.len() as f64;
+        let mut current_scaling_factor = self.scaling_factor;
+
+        let summed_samples: Vec<(f64, f64)> = summed_samples
+            .into_iter()
+            .map(|(mut l, mut r)| {
+                l *= current_scaling_factor;
+                r *= current_scaling_factor;
+                current_scaling_factor -= interpolation_increment;
+                (l, r)
+            })
+            .collect();
+
+        self.scaling_factor = new_scaling_factor;
+
+        // Shuold be removed when applying refactoring changes
+        // for clarity only
+        let f64_to_i16 = |x: f64| x.min(i16::MAX as f64).max(i16::MIN as f64).round() as i16;
 
         summed_samples
             .into_iter()
-            .map(|(l, r)| {
-                (
-                    self.clip_to_i16((l as f64 * scaling_factor) as i64),
-                    self.clip_to_i16((r as f64 * scaling_factor) as i64),
-                )
-            })
+            .map(|(l, r)| (f64_to_i16(l), f64_to_i16(r)))
             .collect()
     }
 
