bb370b9e2f4a803281c47dbb9b9e555ba07996ed
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2025-07-28 16:53:53 +0200

    wip

--- ts/examples/smelter-app/src/index.ts
+++ ts/examples/smelter-app/src/index.ts
@@ -1,19 +1,14 @@
 import { initializeSmelterInstance } from './smelter';
 import { app } from './routes';
-import { initialCleanup, manageHlsToHlsStreams } from './manageHlsToHlsStreams';
 import { manageTwitchChannelInfo } from './manageTwitchChannelInfo';
 
 async function run() {
-  console.log('Stop old FFmpeg processes and remove files.');
-  await initialCleanup();
   console.log('Start monitoring Twitch categories.');
   await manageTwitchChannelInfo();
-  console.log('Run HLS-to-HLS pipeline for each available stream.');
-  await manageHlsToHlsStreams();
   console.log('Start Smelter instance');
   await initializeSmelterInstance();
 
-  app.listen(3001);
+  await app.listen({ port: 3001 });
 }
 
 void run();
