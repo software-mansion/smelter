c458ba8413506ec127535ceb6ca8c9bd3fde0886
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2025-07-28 16:53:53 +0200

    Basic multiroom setup

---
+++ ts/examples/smelter-app/src/server/roomState.ts
@@ -0,0 +1,283 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { SmelterInstance, type RegisterSmelterInputOptions, type SmelterOutput } from '../smelter';
+import { hlsUrlForTwitchChannel } from '../streamlink';
+import { TwitchChannelMonitor } from '../twitch/ChannelMonitor';
+import { sleep } from '../utils';
+import type { InputConfig, Layout } from '../app/store';
+
+export type RoomInputState = {
+  inputId: string;
+  type: 'local-mp4' | 'twitch-channel' | 'kick-channel';
+  status: 'disconnected' | 'pending' | 'connected';
+  volume: number;
+  metadata: {
+    title: string;
+    description: string;
+  };
+} & TypeSpecificState;
+
+type TypeSpecificState =
+  | { type: 'local-mp4'; mp4FilePath: string }
+  | { type: 'twitch-channel'; channelId: string; hlsUrl: string; monitor: TwitchChannelMonitor }
+  | { type: 'kick-channel'; channelId: string; hlsUrl: string };
+
+type UpdateInputOptions = {
+  volume: number;
+};
+
+export type RegisterInputOptions =
+  | {
+      type: 'twitch-channel';
+      twitchChannelId: string;
+    }
+  | {
+      type: 'kick-channel';
+      kickChannelId: string;
+    };
+
+export class RoomState {
+  private inputs: RoomInputState[];
+  private layout: Layout = 'grid';
+  private idPrefix: string;
+
+  private output: SmelterOutput;
+
+  public lastReadTimestamp: number;
+  public creationTimestamp: number;
+  // if room is marked for deletion
+  public pendingDelete?: boolean;
+
+  public constructor(idPrefix: string, output: SmelterOutput) {
+    this.inputs = getInitialInputState(idPrefix);
+    this.idPrefix = idPrefix;
+    this.output = output;
+
+    this.lastReadTimestamp = Date.now();
+    this.creationTimestamp = Date.now();
+
+    setTimeout(async () => {
+      const maybeFirstInput = this.inputs[0];
+      if (maybeFirstInput) {
+        await this.connectInput(maybeFirstInput.inputId);
+      }
+    }, 0);
+  }
+
+  public getWhepUrl(): string {
+    return this.output.url;
+  }
+
+  public getState(): [RoomInputState[], Layout] {
+    this.lastReadTimestamp = Date.now();
+    return [this.inputs, this.layout];
+  }
+
+  public async addNewInput(opts: RegisterInputOptions) {
+    if (opts.type === 'twitch-channel') {
+      const inputId = inputIdForTwitchInput(this.idPrefix, opts.twitchChannelId);
+      if (this.inputs.find(input => input.inputId === inputId)) {
+        throw new Error(`Input for Twitch channel ${opts.twitchChannelId} already exists.`);
+      }
+
+      const hlsUrl = await hlsUrlForTwitchChannel(opts.twitchChannelId);
+      const monitor = await TwitchChannelMonitor.startMonitor(opts.twitchChannelId);
+
+      if (this.inputs.find(input => input.inputId === inputId)) {
+        monitor.stop();
+        throw new Error(`Input for Twitch channel ${opts.twitchChannelId} already exists.`);
+      }
+
+      const inputState: RoomInputState = {
+        inputId,
+        type: `twitch-channel`,
+        status: 'disconnected',
+        metadata: {
+          title: '', // will be populated on update
+          description: '',
+        },
+        volume: 0,
+        channelId: opts.twitchChannelId,
+        hlsUrl,
+        monitor,
+      };
+      monitor.onUpdate((streamInfo, _isLive) => {
+        inputState.metadata.title = `[Twitch.tv/${streamInfo.category}] ${streamInfo.displayName}`;
+        inputState.metadata.description = streamInfo.title;
+        this.updateStoreWithState();
+      });
+      this.inputs.push(inputState);
+      return inputId;
+    } else if (opts.type === 'kick-channel') {
+      throw new Error('Add kick support');
+    }
+  }
+
+  public async removeInput(inputId: string): Promise<void> {
+    const input = this.getInput(inputId);
+    this.inputs = this.inputs.filter(input => input.inputId !== inputId);
+    this.updateStoreWithState();
+
+    while (input.status === 'pending') {
+      await sleep(500);
+    }
+    if (input.status === 'connected') {
+      try {
+        await SmelterInstance.unregisterInput(inputId);
+      } catch (err: any) {
+        console.log(err, 'Failed to unregister when removing input.');
+      }
+      input.status = 'disconnected';
+    }
+  }
+
+  public async connectInput(inputId: string) {
+    const input = this.getInput(inputId);
+    if (input.status !== 'disconnected') {
+      return;
+    }
+    input.status = 'pending';
+    const options = registerOptionsFromInput(input);
+    try {
+      await SmelterInstance.registerInput(inputId, options);
+    } catch (err: any) {
+      input.status = 'disconnected';
+      throw err;
+    }
+    input.status = 'connected';
+    this.updateStoreWithState();
+  }
+
+  public async disconnectInput(inputId: string) {
+    const input = this.getInput(inputId);
+    if (input.status === 'disconnected') {
+      return;
+    }
+    input.status = 'pending';
+    this.updateStoreWithState();
+    try {
+      await SmelterInstance.unregisterInput(inputId);
+    } finally {
+      input.status = 'disconnected';
+      this.updateStoreWithState();
+    }
+  }
+
+  public async updateInput(inputId: string, options: Partial<UpdateInputOptions>) {
+    const input = this.getInput(inputId);
+    input.volume = options.volume ?? input.volume;
+    this.updateStoreWithState();
+  }
+
+  public reorderInputs(inputOrder: string[]) {
+    const inputIdSet = new Set(this.inputs.map(input => input.inputId));
+    const inputs: RoomInputState[] = [];
+    for (const inputId of inputOrder) {
+      const input = this.inputs.find(input => input.inputId === inputId);
+      if (input) {
+        inputs.push(input);
+        inputIdSet.delete(inputId);
+      }
+    }
+    for (const inputId of inputIdSet) {
+      const input = this.inputs.find(input => input.inputId === inputId);
+      if (input) {
+        inputs.push(input);
+      }
+    }
+
+    this.inputs = inputs;
+    this.updateStoreWithState();
+  }
+
+  public updateLayout(layout: Layout) {
+    this.layout = layout;
+    this.updateStoreWithState();
+  }
+
+  public async deleteRoom() {
+    const inputs = this.inputs;
+    this.inputs = [];
+    for (const input of inputs) {
+      try {
+        await SmelterInstance.unregisterInput(input.inputId);
+      } catch (err: any) {
+        console.error('Failed to remove input when removing the room.', err?.body ?? err);
+      }
+    }
+
+    try {
+      await SmelterInstance.unregisterOutput(this.output.id);
+    } catch (err: any) {
+      console.error('Failed to remove output', err?.body ?? err);
+    }
+  }
+
+  private updateStoreWithState() {
+    const inputs: InputConfig[] = this.inputs
+      .filter(input => input.status === 'connected')
+      .map(input => ({
+        inputId: input.inputId,
+        title: input.metadata.title,
+        description: input.metadata.description,
+        volume: input.volume,
+      }));
+    this.output.store.getState().updateState(inputs, this.layout);
+  }
+
+  private getInput(inputId: string): RoomInputState {
+    const input = this.inputs.find(input => input.inputId === inputId);
+    if (!input) {
+      throw new Error(`Input ${inputId} not found`);
+    }
+    return input;
+  }
+}
+
+function registerOptionsFromInput(input: RoomInputState): RegisterSmelterInputOptions {
+  if (input.type === 'local-mp4') {
+    return { type: 'mp4', filePath: input.mp4FilePath };
+  } else if (['twitch-channel', 'kick-channel'].includes(input.type)) {
+    return { type: 'hls', url: input.hlsUrl };
+  } else {
+    throw Error('Unknown type');
+  }
+}
+
+function inputIdForTwitchInput(idPrefix: string, twitchChannelId: string): string {
+  return `${idPrefix}::twitch::${twitchChannelId} `;
+}
+
+function getInitialInputState(idPrefix: string): RoomInputState[] {
+  const inputs: RoomInputState[] = [];
+  const fc25filePath = path.join(process.cwd(), `fc_25_gameplay.mp4`);
+  if (fs.existsSync(fc25filePath)) {
+    inputs.push({
+      inputId: `${idPrefix}::local::fc_25_gameplay`,
+      type: 'local-mp4',
+      status: 'disconnected',
+      metadata: {
+        title: '[MP4] FC 25 Gameplay',
+        description: '[Static source] EA Sports FC 25 Gameplay',
+      },
+      mp4FilePath: fc25filePath,
+      volume: 0,
+    });
+  }
+
+  const nbaFilePath = path.join(process.cwd(), `fc_25_gameplay.mp4`);
+  if (fs.existsSync(path.join(process.cwd(), `nba_gameplay.mp4`))) {
+    inputs.push({
+      inputId: `${idPrefix}::local::nba_gameplay`,
+      type: 'local-mp4',
+      status: 'disconnected',
+      metadata: {
+        title: '[MP4] NBA 2K25 Gameplay',
+        description: '[Static source] NBA 2K25 Gameplay',
+      },
+      mp4FilePath: nbaFilePath,
+      volume: 0,
+    });
+  }
+  return inputs;
+}
