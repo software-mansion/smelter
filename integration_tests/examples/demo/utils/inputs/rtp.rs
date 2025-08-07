5f3584f12ce545c830636aaff987c389cf71be09
Jakub Rękas <jakub.rekas@swmansion.com>
2025-08-07 15:43:58 +0200

    WIP: rtp input registration almost works, start with checking what to do with aac options

--- integration_tests/examples/demo/utils/inputs/rtp.rs
+++ integration_tests/examples/demo/utils/inputs/rtp.rs
@@ -1,38 +1,68 @@
 use anyhow::Result;
-use inquire::{min_length, Select, Text};
-use strum::IntoEnumIterator;
+use inquire::Select;
+use serde_json::json;
+use strum::{Display, EnumIter, IntoEnumIterator};
 
 use crate::utils::{
+    get_free_port,
     inputs::{
         input_name, AudioDecoder, AudioSetupOptions, InputHandler, VideoDecoder, VideoSetupOptions,
     },
-    RegisterOptions, TransportProtocol,
+    TransportProtocol,
 };
+
+#[derive(Debug, Display, EnumIter, Clone)]
+pub enum RtpRegisterOptions {
+    #[strum(to_string = "Set video stream")]
+    SetVideoStream,
+
+    #[strum(to_string = "Set audio stream")]
+    SetAudioStream,
+
+    #[strum(to_string = "Set transport protocol")]
+    SetTransportProtocol,
+
+    #[strum(to_string = "Done")]
+    Done,
+}
 
 #[derive(Debug)]
 pub struct RtpInput {
     name: String,
+    port: u16,
     video: Option<RtpInputVideoOptions>,
     audio: Option<RtpInputAudioOptions>,
-    // transport_protocol: TransportProtocol,
+    transport_protocol: Option<TransportProtocol>,
 }
 
 impl RtpInput {
     pub fn setup() -> Result<Self> {
         let mut rtp_input = Self {
             name: input_name(),
+
+            // TODO: (@jbrs) Make it possible for user
+            // to setup their own port
+            port: get_free_port(),
             video: None,
             audio: None,
+            transport_protocol: None,
         };
 
-        let options = RegisterOptions::iter().collect::<Vec<_>>();
+        let options = RtpRegisterOptions::iter().collect::<Vec<_>>();
         loop {
             let action = Select::new("What to do?", options.clone()).prompt()?;
 
             match action {
-                RegisterOptions::SetVideoStream => rtp_input.setup_video()?,
-                RegisterOptions::SetAudioStream => rtp_input.setup_audio()?,
-                RegisterOptions::Done => break,
+                RtpRegisterOptions::SetVideoStream => rtp_input.setup_video()?,
+                RtpRegisterOptions::SetAudioStream => rtp_input.setup_audio()?,
+                RtpRegisterOptions::SetTransportProtocol => rtp_input.setup_transport_protocol()?,
+                RtpRegisterOptions::Done => {
+                    if rtp_input.video.is_none() && rtp_input.audio.is_none() {
+                        println!("At least one of \"video\" and \"audio\" has to be defined.");
+                    } else {
+                        break;
+                    }
+                }
             }
         }
 
@@ -68,11 +98,33 @@ impl RtpInput {
         }
         Ok(())
     }
+
+    fn setup_transport_protocol(&mut self) -> Result<()> {
+        let options = TransportProtocol::iter().collect();
+
+        let prot = Select::new("Select protocol:", options).prompt()?;
+        self.transport_protocol = Some(prot);
+        Ok(())
+    }
 }
 
 impl InputHandler for RtpInput {
     fn name(&self) -> &str {
         &self.name
+    }
+
+    fn port(&self) -> u16 {
+        self.port
+    }
+
+    fn serialize(&self) -> serde_json::Value {
+        json!({
+            "type": "rtp_stream",
+            "port": self.port,
+            "transport_protocol": self.transport_protocol.as_ref().map(|t| t.to_string()),
+            "video": self.video.as_ref().map(|v| v.serialize()),
+            "audio": self.audio.as_ref().map(|a| a.serialize()),
+        })
     }
 }
 
@@ -88,6 +140,12 @@ impl RtpInputVideoOptions {
         let decoder = Select::new("Select decoder:", options).prompt()?;
         self.decoder = decoder;
         Ok(())
+    }
+
+    pub fn serialize(&self) -> serde_json::Value {
+        json!({
+            "decoder": self.decoder.to_string(),
+        })
     }
 }
 
@@ -111,6 +169,12 @@ impl RtpInputAudioOptions {
         let decoder = Select::new("Select decoder:", options).prompt()?;
         self.decoder = decoder;
         Ok(())
+    }
+
+    pub fn serialize(&self) -> serde_json::Value {
+        json!({
+            "decoder": self.decoder.to_string(),
+        })
     }
 }
 
