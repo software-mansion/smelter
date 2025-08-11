8dc4c64e1a0f75efe6fd75f80906c5b981a3fc69
Mikołaj Radkowski <mikolaj.radkowski@protonmail.com>
2025-08-11 11:10:43 +0200

    Update to rust `1.89` (#1213)
    Co-authored-by: noituri <mikolaj.radkowski@protonmail>

--- compositor_pipeline/src/pipeline/rtp/rtp_output.rs
+++ compositor_pipeline/src/pipeline/rtp/rtp_output.rs
@@ -233,13 +233,13 @@ impl Drop for RtpOutput {
 }
 
 impl Output for RtpOutput {
-    fn audio(&self) -> Option<OutputAudio> {
+    fn audio(&self) -> Option<OutputAudio<'_>> {
         self.audio.as_ref().map(|audio| OutputAudio {
             samples_batch_sender: &audio.sample_batch_sender,
         })
     }
 
-    fn video(&self) -> Option<OutputVideo> {
+    fn video(&self) -> Option<OutputVideo<'_>> {
         self.video.as_ref().map(|video| OutputVideo {
             resolution: video.config.resolution,
             frame_format: video.config.output_format,
