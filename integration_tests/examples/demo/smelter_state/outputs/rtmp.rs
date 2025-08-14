d3cdf2024f733080a5e720f9de315a6075b6a6e7
Jakub Rękas <jakub.rekas@swmansion.com>
2025-08-14 15:18:46 +0200

    Created builder for `RtmpOutput`

--- integration_tests/examples/demo/smelter_state/outputs/rtmp.rs
+++ integration_tests/examples/demo/smelter_state/outputs/rtmp.rs
@@ -1,6 +1,26 @@
+use anyhow::Result;
+use std::{env, process::Child};
+
+use inquire::{Select, Text};
 use serde_json::json;
+use strum::Display;
+use tracing::error;
 
 use crate::smelter_state::outputs::{AudioEncoder, OutputHandler, VideoEncoder, VideoResolution};
+
+pub const SMELTER_RTMP_URL: &str = "SMELTER_RTMP_URL";
+
+#[derive(Debug, Display, Clone)]
+pub enum RtmpRegisterOptions {
+    #[strum(to_string = "Add video stream")]
+    AddVideoStream,
+
+    #[strum(to_string = "Add audio stream")]
+    AddAudioStream,
+
+    #[strum(to_string = "Skip")]
+    Skip,
+}
 
 #[derive(Debug)]
 pub struct RtmpOutput {
@@ -8,6 +28,7 @@ pub struct RtmpOutput {
     url: String,
     video: Option<RtmpOutputVideoOptions>,
     audio: Option<RtmpOutputAudioOptions>,
+    stream_handles: Vec<Child>,
 }
 
 impl OutputHandler for RtmpOutput {
@@ -28,6 +49,117 @@ impl OutputHandler for RtmpOutput {
 
     fn on_after_registration(&mut self) -> anyhow::Result<()> {
         Ok(())
+    }
+}
+
+pub struct RtmpOutputBuilder {
+    name: String,
+    url: String,
+    video: Option<RtmpOutputVideoOptions>,
+    audio: Option<RtmpOutputAudioOptions>,
+}
+
+impl RtmpOutputBuilder {
+    pub fn new() -> Self {
+        Self {
+            name: String::new(),
+            url: String::new(),
+            video: None,
+            audio: None,
+        }
+    }
+
+    pub fn prompt(self) -> Result<Self> {
+        let mut builder = self;
+
+        let url: String = match env::var(SMELTER_RTMP_URL) {
+            Ok(url) => url,
+            Err(_) => builder.prompt_url()?,
+        };
+
+        builder = builder.with_url(url);
+
+        let video_options = vec![
+            RtmpRegisterOptions::AddVideoStream,
+            RtmpRegisterOptions::Skip,
+        ];
+        let audio_options = vec![
+            RtmpRegisterOptions::AddVideoStream,
+            RtmpRegisterOptions::Skip,
+        ];
+
+        loop {
+            let video_selection =
+                Select::new("Add video stream?", video_options.clone()).prompt_skippable()?;
+
+            builder = match video_selection {
+                Some(RtmpRegisterOptions::AddVideoStream) => {
+                    builder.with_video(RtmpOutputVideoOptions::default())
+                }
+                Some(RtmpRegisterOptions::Skip) | None => builder,
+                _ => unreachable!(),
+            };
+
+            let audio_selection =
+                Select::new("Add audio stream?", audio_options.clone()).prompt_skippable()?;
+
+            builder = match audio_selection {
+                Some(RtmpRegisterOptions::AddAudioStream) => {
+                    builder.with_audio(RtmpOutputAudioOptions::default())
+                }
+                Some(RtmpRegisterOptions::Skip) | None => builder,
+                _ => unreachable!(),
+            };
+
+            if builder.video.is_none() && builder.audio.is_none() {
+                error!("At least one video or one audio stream has to be specified!");
+            } else {
+                break;
+            }
+        }
+        Ok(builder)
+    }
+
+    fn prompt_url(&self) -> Result<String> {
+        Ok(Text::new("Url:").prompt()?)
+    }
+
+    pub fn with_url(mut self, url: String) -> Self {
+        let name = format!("output_rtmp_{url}");
+        self.name = name;
+        self.url = url;
+        self
+    }
+
+    pub fn with_video(mut self, video: RtmpOutputVideoOptions) -> Self {
+        self.video = Some(video);
+        self
+    }
+
+    pub fn with_audio(mut self, audio: RtmpOutputAudioOptions) -> Self {
+        self.audio = Some(audio);
+        self
+    }
+
+    fn serialize(&self, inputs: &[&str]) -> serde_json::Value {
+        json!({
+            "type": "rtmp_client",
+            "url": self.url,
+            "video": self.video.as_ref().map(|v| v.serialize_register(inputs)),
+            "audio": self.audio.as_ref().map(|a| a.serialize_register(inputs)),
+        })
+    }
+
+    pub fn build(self, inputs: &[&str]) -> (RtmpOutput, serde_json::Value) {
+        let register_request = self.serialize(inputs);
+        let rtmp_output = RtmpOutput {
+            name: self.name,
+            url: self.url,
+            video: self.video,
+            audio: self.audio,
+            stream_handles: vec![],
+        };
+        (rtmp_output, register_request)
     }
 }
 
@@ -117,8 +249,6 @@ impl RtmpOutputAudioOptions {
             .iter()
             .map(|input_id| {
                 json!({
-                    "type": "input_stream",
-                    "id": input_id,
                     "input_id": input_id,
                 })
             })
@@ -139,8 +269,6 @@ impl RtmpOutputAudioOptions {
             .iter()
             .map(|input_id| {
                 json!({
-                    "type": "input_stream",
-                    "id": input_id,
                     "input_id": input_id,
                 })
             })
