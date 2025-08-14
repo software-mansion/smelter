c114fdde338f8b8ee813e1604e53863e537f551d
Jakub Rękas <jakub.rekas@swmansion.com>
2025-08-14 14:02:45 +0200

    WIP: Serialization for RtmpOutput written (no builder yet)

--- integration_tests/examples/demo/smelter_state/outputs/rtmp.rs
+++ integration_tests/examples/demo/smelter_state/outputs/rtmp.rs
@@ -2,6 +2,7 @@ use serde_json::json;
 
 use crate::smelter_state::outputs::{AudioEncoder, OutputHandler, VideoEncoder, VideoResolution};
 
+#[derive(Debug)]
 pub struct RtmpOutput {
     name: String,
     url: String,
@@ -14,15 +15,23 @@ impl OutputHandler for RtmpOutput {
         &self.name
     }
 
-    fn serialize_register(&self, inputs: &[&str]) -> serde_json::Value {
+    fn serialize_update(&self, inputs: &[&str]) -> serde_json::Value {
         json!({
-            "type": "rtmp_client",
-            "url": self.url,
-
+            "video": self.video.as_ref().map(|v| v.serialize_update(inputs)),
+            "audio": self.audio.as_ref().map(|a| a.serialize_update(inputs)),
         })
+    }
+
+    fn on_before_registration(&mut self) -> anyhow::Result<()> {
+        Ok(())
+    }
+
+    fn on_after_registration(&mut self) -> anyhow::Result<()> {
+        Ok(())
     }
 }
 
+#[derive(Debug)]
 pub struct RtmpOutputVideoOptions {
     resolution: VideoResolution,
     encoder: VideoEncoder,
@@ -30,6 +39,36 @@ pub struct RtmpOutputVideoOptions {
 
 impl RtmpOutputVideoOptions {
     pub fn serialize_register(&self, inputs: &[&str]) -> serde_json::Value {
+        let input_json = inputs
+            .iter()
+            .map(|input_id| {
+                json!({
+                    "type": "input_stream",
+                    "id": input_id,
+                    "input_id": input_id,
+                })
+            })
+            .collect::<Vec<_>>();
+
+        json!({
+            "resolution": self.resolution.serialize(),
+            "encoder" : {
+                "type": self.encoder.to_string(),
+            },
+            "initial": {
+                "root": {
+                    "type": "tiles",
+                    "id": "tiles",
+                    "transition": {
+                        "duration_ms": 500,
+                    },
+                    "children": input_json,
+                },
+            }
+        })
+    }
+
+    pub fn serialize_update(&self, inputs: &[&str]) -> serde_json::Value {
         let input_json = inputs
             .iter()
             .map(|input_id| {
@@ -67,8 +106,50 @@ impl Default for RtmpOutputVideoOptions {
     }
 }
 
+#[derive(Debug)]
 pub struct RtmpOutputAudioOptions {
     encoder: AudioEncoder,
+}
+
+impl RtmpOutputAudioOptions {
+    pub fn serialize_register(&self, inputs: &[&str]) -> serde_json::Value {
+        let input_json = inputs
+            .iter()
+            .map(|input_id| {
+                json!({
+                    "type": "input_stream",
+                    "id": input_id,
+                    "input_id": input_id,
+                })
+            })
+            .collect::<Vec<_>>();
+
+        json!({
+            "encoder": {
+                "type": self.encoder.to_string(),
+            },
+            "initial": {
+                "inputs": input_json,
+            }
+        })
+    }
+
+    pub fn serialize_update(&self, inputs: &[&str]) -> serde_json::Value {
+        let input_json = inputs
+            .iter()
+            .map(|input_id| {
+                json!({
+                    "type": "input_stream",
+                    "id": input_id,
+                    "input_id": input_id,
+                })
+            })
+            .collect::<Vec<_>>();
+
+        json!({
+            "inputs": input_json,
+        })
+    }
 }
 
 impl Default for RtmpOutputAudioOptions {
