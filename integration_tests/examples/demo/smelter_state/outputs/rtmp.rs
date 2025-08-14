08e233417381081125042dd5510e40858f5b4183
Jakub Rękas <jakub.rekas@swmansion.com>
2025-08-14 10:46:47 +0200

    WIP: Created basic structs for RTMP.

--- integration_tests/examples/demo/smelter_state/outputs/rtmp.rs
+++ integration_tests/examples/demo/smelter_state/outputs/rtmp.rs
@@ -1,1 +1,38 @@
+use crate::smelter_state::outputs::{AudioEncoder, VideoEncoder, VideoResolution};
 
+pub struct RtmpOutput {
+    name: String,
+    url: String,
+    video: Option<RtmpOutputVideoOptions>,
+    audio: Option<RtmpOutputAudioOptions>,
+}
+
+pub struct RtmpOutputVideoOptions {
+    resolution: VideoResolution,
+    encoder: VideoEncoder,
+}
+
+impl Default for RtmpOutputVideoOptions {
+    fn default() -> Self {
+        let resolution = VideoResolution {
+            width: 1920,
+            height: 1080,
+        };
+        Self {
+            resolution,
+            encoder: VideoEncoder::FfmpegH264,
+        }
+    }
+}
+
+pub struct RtmpOutputAudioOptions {
+    encoder: AudioEncoder,
+}
+
+impl Default for RtmpOutputAudioOptions {
+    fn default() -> Self {
+        Self {
+            encoder: AudioEncoder::Aac,
+        }
+    }
+}
