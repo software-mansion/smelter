8f86d80a49ab61c7aaf0477e4b667e75146375c5
Jakub Rękas <jakub.rekas@swmansion.com>
2025-08-19 15:55:38 +0200

    Added bin to generate test frequencies mp4 files.

---
+++ integration_tests/src/bin/generate_frequencies.rs
@@ -0,0 +1,14 @@
+use std::process::Command;
+
+fn main() {
+    let encoders = vec!["aac", "libopus"];
+    let notes = vec![("a", 440.0), ("c_sharp", 554.37), ("e", 659.26)];
+
+    for encoder in &encoders {
+        for (name, freq) in &notes {
+            let cmd = format!("ffmpeg -f lavfi -i \"sine=frequency={freq}:sample_rate=48000:duration=20\" -af \"volume=0.2\" -c:a {encoder} -b:a 192k -ar 48000 -vn {name}_{encoder}.mp4");
+
+            Command::new("bash").arg("-c").arg(cmd).status().unwrap();
+        }
+    }
+}
