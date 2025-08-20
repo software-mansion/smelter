b0b8bbffbf3690b1a587609b9004ab013d9e31d2
Jakub Rękas <jakub.rekas@swmansion.com>
2025-08-20 09:44:47 +0200

    Generate stereo dumps audio frequencies

--- integration_tests/src/bin/generate_frequencies.rs
+++ integration_tests/src/bin/generate_frequencies.rs
@@ -6,7 +6,7 @@ fn main() {
 
     for encoder in &encoders {
         for (name, freq) in &notes {
-            let cmd = format!("ffmpeg -f lavfi -i \"sine=frequency={freq}:sample_rate=48000:duration=20\" -af \"volume=0.2\" -c:a {encoder} -b:a 192k -ar 48000 -vn {name}_{encoder}.mp4");
+            let cmd = format!("ffmpeg -f lavfi -i \"sine=frequency={freq}:sample_rate=48000:duration=20\" -af \"volume=0.4\" -c:a {encoder} -b:a 192k -ac 2 -ar 48000 -vn {name}_{encoder}.mp4");
 
             Command::new("bash").arg("-c").arg(cmd).status().unwrap();
         }
