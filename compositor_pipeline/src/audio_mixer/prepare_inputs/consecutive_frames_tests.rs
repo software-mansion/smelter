9f64f290f56b76a5598e2f0f7d7f0a92d620bbe5
Jakub Rękas <74935888+JBRS307@users.noreply.github.com>
2025-09-02 11:54:54 +0200

    Fixed clicking in audio with offset set. (#1268)

--- compositor_pipeline/src/audio_mixer/prepare_inputs/consecutive_frames_tests.rs
+++ compositor_pipeline/src/audio_mixer/prepare_inputs/consecutive_frames_tests.rs
@@ -12,7 +12,8 @@ fn test_continuity_between_frames() {
     let end = start + batch_duration;
     let sample_rate = 48000;
     let sample_duration = Duration::from_secs_f64(1.0 / sample_rate as f64);
-    let small_error = Duration::from_secs_f64(sample_duration.as_secs_f64() * 0.001);
+    let small_error = Duration::from_secs_f64(sample_duration.as_secs_f64() * 0.005);
+    let numerical_error = Duration::from_secs_f64(sample_duration.as_secs_f64() * 0.0005);
     let half_sample = Duration::from_secs_f64(sample_duration.as_secs_f64() * 0.5);
 
     let first_batch = Arc::new(vec![(1.0, 1.0), (2.0, 2.0), (3.0, 3.0), (4.0, 4.0)]);
@@ -259,6 +260,66 @@ fn test_continuity_between_frames() {
     let first_batch_start = start - sample_duration + small_error;
     let second_batch_start = first_batch_start + (4 * sample_duration);
     let third_batch_start = first_batch_start + (8 * sample_duration) + small_error;
+    assert_eq!(
+        frame_input_samples(
+            start,
+            end,
+            vec![
+                InputAudioSamples {
+                    samples: first_batch.clone(),
+                    start_pts: first_batch_start,
+                    end_pts: first_batch_start + (4 * sample_duration)
+                },
+                InputAudioSamples {
+                    samples: second_batch.clone(),
+                    start_pts: second_batch_start,
+                    end_pts: second_batch_start + (4 * sample_duration)
+                },
+            ],
+            sample_rate
+        ),
+        vec![
+            (1.0, 1.0),
+            (2.0, 2.0),
+            (3.0, 3.0),
+            (4.0, 4.0),
+            (5.0, 5.0),
+            (6.0, 6.0)
+        ]
+    );
+    assert_eq!(
+        frame_input_samples(
+            start + batch_duration,
+            end + batch_duration,
+            vec![
+                InputAudioSamples {
+                    samples: second_batch.clone(),
+                    start_pts: second_batch_start,
+                    end_pts: second_batch_start + (4 * sample_duration)
+                },
+                InputAudioSamples {
+                    samples: third_batch.clone(),
+                    start_pts: third_batch_start,
+                    end_pts: third_batch_start + (4 * sample_duration)
+                }
+            ],
+            sample_rate
+        ),
+        vec![
+            (7.0, 7.0),
+            (8.0, 8.0),
+            (9.0, 9.0),
+            (10.0, 10.0),
+            (11.0, 11.0),
+            (12.0, 12.0)
+        ]
+    );
+
+    // Shifted only by numerical error
+    // It should behave as if it was not shifted at all
+    let first_batch_start = start + numerical_error;
+    let second_batch_start = first_batch_start + (4 * sample_duration) - numerical_error;
+    let third_batch_start = first_batch_start + (8 * sample_duration) + numerical_error;
     assert_eq!(
         frame_input_samples(
             start,
