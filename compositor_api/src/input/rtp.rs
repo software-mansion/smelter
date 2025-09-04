5c429921aed57a8afb9d6a09094cce2ea1cd8fab
Wojciech Kaźmierczak <wojciech.kazmierczak@swmansion.com>
2025-09-04 14:32:30 +0200

    resolve rtp decoder preferences in pipeline

--- compositor_api/src/input/rtp.rs
+++ compositor_api/src/input/rtp.rs
@@ -61,5 +61,28 @@ pub enum InputRtpAudioOptions {
 #[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
 #[serde(rename_all = "snake_case", deny_unknown_fields)]
 pub struct InputRtpVideoOptions {
-    pub decoder: VideoDecoder,
+    pub decoder: RtpVideoDecoderOptions,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
+#[serde(rename_all = "snake_case", deny_unknown_fields)]
+pub enum RtpVideoDecoderOptions {
+    /// Use the software h264 decoder based on ffmpeg.
+    FfmpegH264,
+
+    /// Use the software vp8 decoder based on ffmpeg.
+    FfmpegVp8,
+
+    /// Use the software vp9 decoder based on ffmpeg.
+    FfmpegVp9,
+
+    /// Use hardware decoder based on Vulkan Video.
+    ///
+    /// This should be faster and more scalable than teh ffmpeg decoder, if the hardware and OS
+    /// support it.
+    ///
+    /// This requires hardware that supports Vulkan Video. Another requirement is this program has
+    /// to be compiled with the `vk-video` feature enabled (enabled by default on platforms which
+    /// support Vulkan, i.e. non-Apple operating systems and not the web).
+    VulkanH264,
 }
