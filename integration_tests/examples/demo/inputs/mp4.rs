c12440a5682f4f4a90ed81694db9552d851338cf
Jakub Rękas <74935888+JBRS307@users.noreply.github.com>
2025-09-10 14:56:38 +0200

    [examples] Added `URL` input source to `MP4` input in interactive example. (#1319)

--- integration_tests/examples/demo/inputs/mp4.rs
+++ integration_tests/examples/demo/inputs/mp4.rs
@@ -1,4 +1,4 @@
-use std::{env, path::PathBuf};
+use std::{env, path::PathBuf, str::FromStr};
 
 use anyhow::Result;
 use inquire::{Confirm, Text};
@@ -15,7 +15,7 @@ use crate::{
     utils::resolve_path,
 };
 
-const MP4_INPUT_PATH: &str = "MP4_INPUT_PATH";
+const MP4_INPUT_SOURCE: &str = "MP4_INPUT_SOURCE";
 
 #[derive(Debug)]
 pub struct Mp4Input {
@@ -28,9 +28,37 @@ impl InputHandler for Mp4Input {
     }
 }
 
+pub enum Mp4InputSource {
+    Path(PathBuf),
+    Url(String),
+}
+
+impl Mp4InputSource {
+    fn serialize(&self) -> (String, String) {
+        match self {
+            Self::Url(url) => ("url".to_string(), url.to_string()),
+            Self::Path(path) => ("path".to_string(), path.to_str().unwrap().to_string()),
+        }
+    }
+}
+
+impl FromStr for Mp4InputSource {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let s = s.trim();
+        if s.starts_with("http") {
+            Ok(Self::Url(s.to_string()))
+        } else {
+            let path = resolve_path(s.into())?;
+            Ok(Self::Path(path))
+        }
+    }
+}
+
 pub struct Mp4InputBuilder {
     name: String,
-    path: Option<PathBuf>,
+    source: Option<Mp4InputSource>,
     r#loop: bool,
 }
 
@@ -40,7 +68,7 @@ impl Mp4InputBuilder {
         let name = format!("mp4_input_{suffix}");
         Self {
             name,
-            path: None,
+            source: None,
             r#loop: false,
         }
     }
@@ -48,7 +76,7 @@ impl Mp4InputBuilder {
     pub fn prompt(self) -> Result<Self> {
         let mut builder = self;
 
-        builder = builder.prompt_path()?;
+        builder = builder.prompt_source()?;
 
         let loop_selection = Confirm::new("Loop input [y/n]:").prompt_skippable()?;
         builder = match loop_selection {
@@ -59,27 +87,30 @@ impl Mp4InputBuilder {
         Ok(builder)
     }
 
-    pub fn prompt_path(self) -> Result<Self> {
-        let env_path = env::var(MP4_INPUT_PATH).unwrap_or_default();
+    pub fn prompt_source(self) -> Result<Self> {
+        let env_source = env::var(MP4_INPUT_SOURCE).unwrap_or_default();
         let default_path = examples_root_dir().join(BUNNY_H264_PATH);
 
         loop {
-            let path_input = Text::new(&format!(
-                "Input path (ESC for {}):",
+            let source_input = Text::new(&format!(
+                "Input path or url (ESC for {}):",
                 default_path.to_str().unwrap(),
             ))
-            .with_initial_value(&env_path)
             .with_autocomplete(FilePathCompleter::default())
+            .with_initial_value(&env_source)
             .prompt_skippable()?;
 
-            match path_input {
-                Some(path) if !path.trim().is_empty() => {
-                    let path = resolve_path(path.into())?;
-                    if path.exists() {
-                        break Ok(self.with_path(path));
-                    } else {
-                        error!("Path is not valid");
-                    }
+            match source_input {
+                Some(source_str) if !source_str.trim().is_empty() => {
+                    let source = Mp4InputSource::from_str(&source_str)?;
+                    match &source {
+                        Mp4InputSource::Path(path) if !path.exists() => {
+                            error!("Path is not valid")
+                        }
+                        Mp4InputSource::Url(_) | Mp4InputSource::Path(_) => {
+                            break Ok(self.with_source(source))
+                        }
+                    };
                 }
                 Some(_) | None => {
                     info!(
@@ -90,14 +121,15 @@ impl Mp4InputBuilder {
                         url: BUNNY_H264_URL.to_string(),
                         path: default_path.clone(),
                     })?;
-                    break Ok(self.with_path(default_path));
+                    let source = Mp4InputSource::Path(default_path);
+                    break Ok(self.with_source(source));
                 }
             }
         }
     }
 
-    pub fn with_path(mut self, path: PathBuf) -> Self {
-        self.path = Some(path);
+    pub fn with_source(mut self, source: Mp4InputSource) -> Self {
+        self.source = Some(source);
         self
     }
 
@@ -107,9 +139,11 @@ impl Mp4InputBuilder {
     }
 
     fn serialize(&self) -> serde_json::Value {
+        let source = self.source.as_ref().unwrap();
+        let (source_key, source_val) = source.serialize();
         json!({
             "type": "mp4",
-            "path": self.path.as_ref().unwrap(),
+            source_key: source_val,
             "loop": self.r#loop,
         })
     }
