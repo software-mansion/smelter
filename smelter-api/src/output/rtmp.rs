4eac9675bdbc838aa8a7941e4837af1441ce1f78
Wojciech Kaźmierczak <wojciech.kazmierczak@swmansion.com>
2025-09-11 12:31:11 +0200

    resolve rtmp encoder options based on vulkan support

--- smelter-api/src/output/rtmp.rs
+++ smelter-api/src/output/rtmp.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -8,11 +8,47 @@ use crate::*;
 #[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
 #[serde(deny_unknown_fields)]
 pub struct RtmpOutput {
+    /// RTMP endpoint url.
     pub url: Arc<str>,
     /// Video stream configuration.
-    pub video: Option<OutputVideoOptions>,
+    pub video: Option<OutputRtmpClientVideoOptions>,
     /// Audio stream configuration.
     pub audio: Option<OutputRtmpClientAudioOptions>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
+#[serde(deny_unknown_fields)]
+pub struct OutputRtmpClientVideoOptions {
+    /// Output resolution in pixels.
+    pub resolution: Resolution,
+    /// Condition for termination of the output stream based on the input streams states. If output includes both audio and video streams, then EOS needs to be sent for every type.
+    pub send_eos_when: Option<OutputEndCondition>,
+    /// Video encoder options.
+    pub encoder: RtmpClientVideoEncoderOptions,
+    /// Root of a component tree/scene that should be rendered for the output. Use [`update_output` request](../routes.md#update-output) to update this value after registration. [Learn more](../../concept/component.md).
+    pub initial: VideoScene,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
+#[serde(tag = "type", rename_all = "snake_case", deny_unknown_fields)]
+pub enum RtmpClientVideoEncoderOptions {
+    #[serde(rename = "ffmpeg_h264")]
+    FfmpegH264 {
+        /// (**default=`"fast"`**) Video output encoder preset. Visit `FFmpeg` [docs](https://trac.ffmpeg.org/wiki/Encode/H.264#Preset) to learn more.
+        preset: Option<H264EncoderPreset>,
+
+        /// (**default=`"yuv420p"`**) Encoder pixel format
+        pixel_format: Option<PixelFormat>,
+
+        /// Raw FFmpeg encoder options. See [docs](https://ffmpeg.org/ffmpeg-codecs.html) for more.
+        ffmpeg_options: Option<HashMap<Arc<str>, Arc<str>>>,
+    },
+    #[serde(rename = "vulkan_h264")]
+    VulkanH264 {
+        /// Encoding bitrate. If not provided, bitrate is calculated based on resolution and framerate.
+        /// For example at 1080p 30 FPS the average bitrate is 5000 kbit/s and max bitrate is 6250 kbit/s.
+        bitrate: Option<VulkanH264EncoderBitrate>,
+    },
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
@@ -20,11 +56,11 @@ pub struct RtmpOutput {
 pub struct OutputRtmpClientAudioOptions {
     /// (**default="sum_clip"**) Specifies how audio should be mixed.
     pub mixing_strategy: Option<AudioMixingStrategy>,
-    /// Condition for termination of output stream based on the input streams states.
+    /// Condition for termination of the output stream based on the input streams states. If output includes both audio and video streams, then EOS needs to be sent for every type.
     pub send_eos_when: Option<OutputEndCondition>,
     /// Audio encoder options.
     pub encoder: RtmpClientAudioEncoderOptions,
-    /// Specifies channels configuration.
+    /// Channels configuration.
     pub channels: Option<AudioChannels>,
     /// Initial audio mixer configuration for output.
     pub initial: AudioScene,
--- smelter-core/src/pipeline/rtmp/rtmp_output.rs
+++ smelter-core/src/pipeline/rtmp/rtmp_output.rs
@@ -143,6 +143,11 @@ impl RtmpClientOutput {
                 )?
             }
             VideoEncoderOptions::VulkanH264(options) => {
+                if !ctx.graphics_context.has_vulkan_encoder_support() {
+                    return Err(OutputInitError::EncoderError(
+                        EncoderInitError::VulkanContextRequiredForVulkanEncoder,
+                    ));
+                }
                 VideoEncoderThread::<VulkanH264Encoder>::spawn(
                     output_id.clone(),
                     VideoEncoderThreadOptions {
