fc99f7a4f3174924201d35b139d8ed988e69e0b6
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2025-09-12 14:55:05 +0200

    log scene updates, but with less details

--- smelter-core/src/pipeline/instance.rs
+++ smelter-core/src/pipeline/instance.rs
@@ -270,7 +270,7 @@ impl Pipeline {
             return Err(UpdateSceneError::AudioVideoNotMatching(output_id));
         };
 
-        info!(?output_id, "Update scene {:?}", scene_root);
+        info!(?output_id, "Update scene");
 
         self.renderer.update_scene(
             output_id,
@@ -298,7 +298,7 @@ impl Pipeline {
             return Ok(());
         }
 
-        info!(?output_id, "Update audio mixer {:?}", audio);
+        info!(?output_id, "Update audio mixer");
         self.audio_mixer.update_output(output_id, audio)
     }
 
