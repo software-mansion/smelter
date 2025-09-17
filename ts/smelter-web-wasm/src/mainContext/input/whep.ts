9c2992d39a3ec21205a3821c6749d3c793730cb6
piotrsnow <122814321+piotrsnow@users.noreply.github.com>
2025-09-17 14:55:04 +0200

    New demo v012: Handling Multiple Mp4 (#1356)
    Co-authored-by: Piotr Ruszczak <piotr.ruszczak@swmansion.com>

--- ts/smelter-web-wasm/src/mainContext/input/whep.ts
+++ ts/smelter-web-wasm/src/mainContext/input/whep.ts
@@ -78,7 +78,7 @@ export async function handleRegisterWhepClientInput(
 
   pc.ontrack = event => {
     const track = event.track;
-    ctx.logger.debug('Received track:', track.kind);
+    ctx.logger.debug(`Received track:  ${track.kind}`);
     inputStream.addTrack(track);
     receivedTracks.push(track);
   };
