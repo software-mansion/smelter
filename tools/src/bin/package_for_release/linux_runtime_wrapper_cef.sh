e8402d3d8b14f606bb1aee508f36ae5627464cd4
Jakub Rękas <jakub.rekas@swmansion.com>
2025-09-26 13:54:17 +0200

    Added linux wrapper script if if no cef specified

--- tools/src/bin/package_for_release/linux_runtime_wrapper_cef.sh
+++ tools/src/bin/package_for_release/linux_runtime_wrapper_cef.sh
@@ -4,8 +4,6 @@
 #   Runtime wrapper which provides paths to native libs used by the web renderer
 #  
 
-set -eo pipefail
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export LD_LIBRARY_PATH="$SCRIPT_DIR/lib:$LD_LIBRARY_PATH:$SCRIPT_DIR/ffmpeg_lib"
