b3c544c104c26f6508907fe266ebde714915d0a9
Jakub Rękas <jakub.rekas@swmansion.com>
2025-10-01 13:57:52 +0200

    Variable frequency snapshots generation

--- integration-tests/src/bin/generate_frequencies.rs
+++ integration-tests/src/bin/generate_frequencies.rs
@@ -1,6 +1,6 @@
-use std::process::Command;
+use std::{fs, process::Command};
 
-use integration_tests::paths::submodule_root_path;
+use integration_tests::paths::{integration_tests_root, submodule_root_path};
 
 enum Encoder {
     Aac,
@@ -27,7 +27,7 @@ fn main() {
     let encoders = vec![Encoder::Aac, Encoder::Opus];
     let notes = vec![("a", 440.0), ("c_sharp", 554.37), ("e", 659.26)];
 
-    let cmd_wd = submodule_root_path()
+    let snapshots_dir = submodule_root_path()
         .join("rtp_packet_dumps")
         .join("inputs");
 
@@ -43,18 +43,86 @@ fn main() {
             Command::new("bash")
                 .arg("-c")
                 .arg(cmd)
-                .current_dir(&cmd_wd)
+                .current_dir(&snapshots_dir)
                 .status()
                 .unwrap();
 
             Command::new("bash")
                 .arg("-c")
                 .arg(format!(
-                    "cargo run -p integration_tests --bin generate_rtp_from_file audio-{file_suffix} {file_name}"
+                    "cargo run -p integration-tests --bin generate_rtp_from_file audio-{file_suffix} {file_name}"
                 ))
-                .current_dir(&cmd_wd)
+                .current_dir(&snapshots_dir)
                 .status()
                 .unwrap();
         }
     }
+
+    let wav_a = "ffmpeg -y -f lavfi -i \"sine=frequency=440.0:sample_rate=48000:duration=5\" -af \"volume=3\" -c:a pcm_s16le -ac 2 -ar 48000 -vn a.wav";
+    let wav_c_sharp = "ffmpeg -y -f lavfi -i \"sine=frequency=554.37:sample_rate=48000:duration=15\" -af \"volume=3\" -c:a pcm_s16le -ac 2 -ar 48000 -vn c_sharp.wav";
+
+    Command::new("bash")
+        .arg("-c")
+        .arg(wav_a)
+        .current_dir(integration_tests_root())
+        .status()
+        .unwrap();
+    Command::new("bash")
+        .arg("-c")
+        .arg(wav_c_sharp)
+        .current_dir(integration_tests_root())
+        .status()
+        .unwrap();
+
+    Command::new("bash")
+        .arg("-c")
+        .arg("sox a.wav c_sharp.wav variable_frequency.wav")
+        .current_dir(integration_tests_root())
+        .status()
+        .unwrap();
+
+    fs::remove_file(integration_tests_root().join("a.wav")).unwrap();
+    fs::remove_file(integration_tests_root().join("c_sharp.wav")).unwrap();
+
+    for encoder in encoders {
+        let file_suffix = encoder.file_name();
+        let encoder_name = encoder.name();
+
+        let file_name = format!("variable_frequency_{file_suffix}.mp4");
+
+        let cmd = format!("ffmpeg -y -i variable_frequency.wav -c:a {encoder_name} -ac 2 -ar 48000 -vn {file_name}");
+
+        Command::new("bash")
+            .arg("-c")
+            .arg(&cmd)
+            .current_dir(integration_tests_root())
+            .status()
+            .unwrap();
+
+        Command::new("bash")
+            .arg("-c")
+            .arg(format!(
+                "cargo run -p integration-tests --bin generate_rtp_from_file audio-{file_suffix} {file_name}"
+            ))
+            .current_dir(integration_tests_root())
+            .status()
+            .unwrap();
+
+        fs::rename(
+            integration_tests_root().join(&file_name),
+            snapshots_dir.join(&file_name),
+        )
+        .unwrap();
+    }
+    fs::remove_file(integration_tests_root().join("variable_frequency.wav")).unwrap();
+    fs::rename(
+        integration_tests_root().join("variable_frequency_aac_audio_aac.rtp"),
+        snapshots_dir.join("variable_frequency_aac_audio_aac.rtp"),
+    )
+    .unwrap();
+    fs::rename(
+        integration_tests_root().join("variable_frequency_opus_audio.rtp"),
+        snapshots_dir.join("variable_frequency_opus_audio.rtp"),
+    )
+    .unwrap();
 }
