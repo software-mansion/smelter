d17c53ec46d0a3373d0ebc23b4823fed7821d697
Jakub Rękas <jakub.rekas@swmansion.com>
2025-10-07 10:38:26 +0200

    Added streamlink resolution to hls input

--- integration-tests/examples/demo/inputs/hls.rs
+++ integration-tests/examples/demo/inputs/hls.rs
@@ -1,14 +1,22 @@
-use std::env;
+use std::{env, process::Command};
 
 use anyhow::Result;
 use inquire::{Select, Text};
 use rand::RngCore;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use strum::{Display, EnumIter, IntoEnumIterator};
+use tracing::error;
 
 use crate::inputs::{InputHandle, VideoDecoder};
 
 const HLS_INPUT_URL: &str = "HLS_INPUT_URL";
+
+#[derive(Debug, EnumIter, Display)]
+enum UrlInputOptions {
+    Streamlink,
+    Manual,
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct HlsInput {
@@ -58,13 +66,41 @@ impl HlsInputBuilder {
     fn prompt_url(self) -> Result<Self> {
         const DEFAULT_URL: &str = "https://raw.githubusercontent.com/membraneframework/membrane_http_adaptive_stream_plugin/master/test/membrane_http_adaptive_stream/integration_test/fixtures/audio_multiple_video_tracks/index.m3u8";
         let env_url = env::var(HLS_INPUT_URL).unwrap_or_default();
-        let hls_url = Text::new("HLS input url (ESC for default):")
-            .with_default(DEFAULT_URL)
-            .with_initial_value(&env_url)
-            .prompt_skippable()?;
-        match hls_url {
-            Some(url) => Ok(self.with_url(url)),
-            None => Ok(self.with_url(DEFAULT_URL.to_string())),
+        loop {
+            let hls_url = Text::new("HLS input url (ESC for default):")
+                .with_initial_value(&env_url)
+                .prompt_skippable()?;
+
+            match hls_url {
+                Some(url) => {
+                    let url_options = UrlInputOptions::iter().collect();
+                    let url_selection =
+                        Select::new("How to resolve URL? (ESC for Streamlink)", url_options)
+                            .prompt_skippable()?;
+                    match url_selection {
+                        Some(UrlInputOptions::Streamlink) => {
+                            let streamlink_result = Command::new("streamlink")
+                                .args(["--stream-url", &url])
+                                .output();
+                            let stream_url = match streamlink_result {
+                                Ok(streamlink_output) => {
+                                    String::from_utf8(streamlink_output.stdout)?
+                                        .trim()
+                                        .to_string()
+                                }
+                                Err(error) => {
+                                    error!(%error, "streamlink command failed");
+                                    println!("If streamlink is not installed please use manual URL resolution");
+                                    continue;
+                                }
+                            };
+                            return Ok(self.with_url(stream_url));
+                        }
+                        Some(UrlInputOptions::Manual) | None => return Ok(self.with_url(url)),
+                    }
+                }
+                None => return Ok(self.with_url(DEFAULT_URL.to_string())),
+            }
         }
     }
 
