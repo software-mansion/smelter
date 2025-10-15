105121080c0096f57dfda48842f843eeb288c484
Jakub Rękas <jakub.rekas@swmansion.com>
2025-10-15 10:17:56 +0200

    Added script to quickly prepare test

---
+++ simpler_test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cargo build -p integration-tests --example simpler
+
+cargo run -p tools --bin package_for_release
+
+mkdir -p ~/smelter-test/
+
+cp ./tools/build/smelter/dependency_check ~/smelter-test/
+cp -r ./tools/build/smelter/smelter.app/ ~/smelter-test/
+cp ./target/release/examples/simpler ~/smelter-test/
+
