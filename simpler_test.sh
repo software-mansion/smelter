f5c37c9623f2697db73c28513df2dcff5d5840d9
Jakub Rękas <jakub.rekas@swmansion.com>
2025-10-15 10:45:28 +0200

    Another script change

--- simpler_test.sh
+++ simpler_test.sh
@@ -9,6 +9,6 @@ cargo run -p tools --bin package_for_release
 mkdir -p ~/smelter-test/
 
 cp ./tools/build/smelter/dependency_check ~/smelter-test/
-cp -r ./tools/build/smelter/smelter.app/ ~/smelter-test/
+cp -r ./tools/build/smelter/smelter.app ~/smelter-test/
 cp ./target/release/examples/simpler ~/smelter-test/
 
