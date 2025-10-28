beefd9818ac7a5f6abea1c45af79f4e234fb144c
Wojciech Kaźmierczak <wojciech.kazmierczak@swmansion.com>
2025-10-28 16:47:14 +0100

    Resolve RTMP encoder options based on vulkan support (#1334)

--- smelter-api/src/output/rtmp_into.rs
+++ smelter-api/src/output/rtmp_into.rs
@@ -13,7 +13,25 @@ impl TryFrom<RtmpOutput> for core::RegisterOutputOptions {
             ));
         }
 
-        let (video_encoder_options, output_video_options) = maybe_video_options_h264_only(video)?;
+        let (video_encoder_options, output_video_options) = match video {
+            Some(OutputRtmpClientVideoOptions {
+                resolution,
+                send_eos_when,
+                encoder,
+                initial,
+            }) => {
+                let output_options = core::RegisterOutputVideoOptions {
+                    initial: initial.try_into()?,
+                    end_condition: send_eos_when.unwrap_or_default().try_into()?,
+                };
+
+                (
+                    Some(encoder.to_pipeline_options(resolution)?),
+                    Some(output_options),
+                )
+            }
+            None => (None, None),
+        };
         let (audio_encoder_options, output_audio_options) = match audio {
             Some(OutputRtmpClientAudioOptions {
                 mixing_strategy,
@@ -22,28 +40,20 @@ impl TryFrom<RtmpOutput> for core::RegisterOutputOptions {
                 channels,
                 initial,
             }) => {
-                let (audio_encoder_options, resolved_channels) = match encoder {
-                    RtmpClientAudioEncoderOptions::Aac { sample_rate } => {
-                        let resolved_channels = channels.unwrap_or(AudioChannels::Stereo);
-                        (
-                            core::AudioEncoderOptions::FdkAac(core::FdkAacEncoderOptions {
-                                channels: resolved_channels.into(),
-                                sample_rate: sample_rate.unwrap_or(44100),
-                            }),
-                            resolved_channels,
-                        )
-                    }
-                };
+                let channels = channels.unwrap_or(AudioChannels::Stereo);
                 let output_audio_options = core::RegisterOutputAudioOptions {
                     initial: initial.try_into()?,
                     end_condition: send_eos_when.unwrap_or_default().try_into()?,
                     mixing_strategy: mixing_strategy
                         .unwrap_or(AudioMixingStrategy::SumClip)
                         .into(),
-                    channels: resolved_channels.into(),
+                    channels: channels.into(),
                 };
 
-                (Some(audio_encoder_options), Some(output_audio_options))
+                (
+                    Some(encoder.to_pipeline_options(channels)),
+                    Some(output_audio_options),
+                )
             }
             None => (None, None),
         };
@@ -62,57 +72,46 @@ impl TryFrom<RtmpOutput> for core::RegisterOutputOptions {
     }
 }
 
-fn maybe_video_options_h264_only(
-    options: Option<OutputVideoOptions>,
-) -> Result<
-    (
-        Option<core::VideoEncoderOptions>,
-        Option<core::RegisterOutputVideoOptions>,
-    ),
-    TypeError,
-> {
-    let Some(options) = options else {
-        return Ok((None, None));
-    };
+impl RtmpClientVideoEncoderOptions {
+    fn to_pipeline_options(
+        &self,
+        resolution: Resolution,
+    ) -> Result<core::VideoEncoderOptions, TypeError> {
+        let encoder_options = match self {
+            RtmpClientVideoEncoderOptions::FfmpegH264 {
+                preset,
+                pixel_format,
+                ffmpeg_options,
+            } => core::VideoEncoderOptions::FfmpegH264(core::FfmpegH264EncoderOptions {
+                preset: preset.unwrap_or(H264EncoderPreset::Fast).into(),
+                resolution: resolution.into(),
+                pixel_format: pixel_format.unwrap_or(PixelFormat::Yuv420p).into(),
+                raw_options: ffmpeg_options
+                    .clone()
+                    .unwrap_or_default()
+                    .into_iter()
+                    .collect(),
+            }),
+            RtmpClientVideoEncoderOptions::VulkanH264 { bitrate } => {
+                core::VideoEncoderOptions::VulkanH264(core::VulkanH264EncoderOptions {
+                    resolution: resolution.into(),
+                    bitrate: bitrate.map(|bitrate| bitrate.try_into()).transpose()?,
+                })
+            }
+        };
+        Ok(encoder_options)
+    }
+}
 
-    let encoder_options = match options.encoder {
-        VideoEncoderOptions::FfmpegH264 {
-            preset,
-            pixel_format,
-            ffmpeg_options,
-        } => core::VideoEncoderOptions::FfmpegH264(core::FfmpegH264EncoderOptions {
-            preset: preset.unwrap_or(H264EncoderPreset::Fast).into(),
-            resolution: options.resolution.into(),
-            pixel_format: pixel_format.unwrap_or(PixelFormat::Yuv420p).into(),
-            raw_options: ffmpeg_options.unwrap_or_default().into_iter().collect(),
-        }),
-        #[cfg(feature = "vk-video")]
-        VideoEncoderOptions::VulkanH264 { bitrate } => {
-            core::VideoEncoderOptions::VulkanH264(core::VulkanH264EncoderOptions {
-                resolution: options.resolution.into(),
-                bitrate: bitrate.map(|bitrate| bitrate.try_into()).transpose()?,
-            })
+impl RtmpClientAudioEncoderOptions {
+    fn to_pipeline_options(&self, channels: AudioChannels) -> core::AudioEncoderOptions {
+        match self {
+            RtmpClientAudioEncoderOptions::Aac { sample_rate } => {
+                core::AudioEncoderOptions::FdkAac(core::FdkAacEncoderOptions {
+                    channels: channels.into(),
+                    sample_rate: sample_rate.unwrap_or(44100),
+                })
+            }
         }
-        #[cfg(not(feature = "vk-video"))]
-        VideoEncoderOptions::VulkanH264 { .. } => {
-            return Err(TypeError::new(super::NO_VULKAN_VIDEO));
-        }
-        VideoEncoderOptions::FfmpegVp8 { .. } => {
-            return Err(TypeError::new(
-                "VP8 output not supported for given protocol",
-            ));
-        }
-        VideoEncoderOptions::FfmpegVp9 { .. } => {
-            return Err(TypeError::new(
-                "VP9 output not supported for given protocol",
-            ));
-        }
-    };
-
-    let output_options = core::RegisterOutputVideoOptions {
-        initial: options.initial.try_into()?,
-        end_condition: options.send_eos_when.unwrap_or_default().try_into()?,
-    };
-
-    Ok((Some(encoder_options), Some(output_options)))
+    }
 }
