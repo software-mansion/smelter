bf262a1b053272b5e3583d25e3ce4316c9eadd22
Wojciech Kaźmierczak <wojciech.kazmierczak@swmansion.com>
2025-10-29 12:30:58 +0100

    Resolve MP4 encoder options based on `vulkan` support (#1475)

--- smelter-core/src/pipeline/mp4/mp4_output.rs
+++ smelter-core/src/pipeline/mp4/mp4_output.rs
@@ -168,6 +168,11 @@ impl Mp4Output {
                 )?
             }
             VideoEncoderOptions::VulkanH264(options) => {
+                if !ctx.graphics_context.has_vulkan_encoder_support() {
+                    return Err(OutputInitError::EncoderError(
+                        EncoderInitError::VulkanContextRequiredForVulkanEncoder,
+                    ));
+                }
                 VideoEncoderThread::<VulkanH264Encoder>::spawn(
                     output_id.clone(),
                     VideoEncoderThreadOptions {
