ba48f26f24ecc22f7711f8aa8468c3b7f2b037ac
Wojciech Kaźmierczak <wojciech.kazmierczak@swmansion.com>
2025-10-29 12:41:48 +0100

    Resolve RTP encoder options based on `vulkan` support (#1333)

--- smelter-api/src/output/rtp.rs
+++ smelter-api/src/output/rtp.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
 
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -12,26 +12,72 @@ pub struct RtpOutput {
     ///   - `udp` - An UDP port number that RTP packets will be sent to.
     ///   - `tcp_server` - A local TCP port number or a port range that Smelter will listen for incoming connections.
     pub port: PortOrPortRange,
-    /// Only valid if `transport_protocol="udp"`. IP address where RTP packets should be sent to.
+    /// IP address to which RTP packets should be sent. This field is only valid if `transport_protocol` field is set to `udp`.
     pub ip: Option<Arc<str>>,
     /// (**default=`"udp"`**) Transport layer protocol that will be used to send RTP packets.
     pub transport_protocol: Option<TransportProtocol>,
-    /// Video stream configuration.
-    pub video: Option<OutputVideoOptions>,
-    /// Audio stream configuration.
+    /// Parameters of a video included in the RTP stream.
+    pub video: Option<OutputRtpVideoOptions>,
+    /// Parameters of an audio included in the RTP stream.
     pub audio: Option<OutputRtpAudioOptions>,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
 #[serde(deny_unknown_fields)]
+pub struct OutputRtpVideoOptions {
+    /// Output resolution in pixels.
+    pub resolution: Resolution,
+    /// Condition for termination of the output stream based on the input streams states. If output includes both audio and video streams, then EOS needs to be sent for every type.
+    pub send_eos_when: Option<OutputEndCondition>,
+    /// Video encoder options.
+    pub encoder: RtpVideoEncoderOptions,
+    /// Root of a component tree/scene that should be rendered for the output. Use [`update_output` request](../routes.md#update-output) to update this value after registration. [Learn more](../../concept/component.md).
+    pub initial: VideoScene,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
+#[serde(tag = "type", rename_all = "snake_case", deny_unknown_fields)]
+pub enum RtpVideoEncoderOptions {
+    #[serde(rename = "ffmpeg_h264")]
+    FfmpegH264 {
+        /// (**default=`"fast"`**) Video output encoder preset. Visit `FFmpeg` [docs](https://trac.ffmpeg.org/wiki/Encode/H.264#Preset) to learn more.
+        preset: Option<H264EncoderPreset>,
+
+        /// (**default=`"yuv420p"`**) Encoder pixel format.
+        pixel_format: Option<PixelFormat>,
+
+        /// Raw FFmpeg encoder options. Visit [docs](https://ffmpeg.org/ffmpeg-codecs.html) to learn more.
+        ffmpeg_options: Option<HashMap<Arc<str>, Arc<str>>>,
+    },
+    #[serde(rename = "ffmpeg_vp8")]
+    FfmpegVp8 {
+        /// Raw FFmpeg encoder options. Visit [docs](https://ffmpeg.org/ffmpeg-codecs.html) to learn more.
+        ffmpeg_options: Option<HashMap<Arc<str>, Arc<str>>>,
+    },
+    #[serde(rename = "ffmpeg_vp9")]
+    FfmpegVp9 {
+        /// (**default=`"yuv420p"`**) Encoder pixel format.
+        pixel_format: Option<PixelFormat>,
+        /// Raw FFmpeg encoder options. Visit [docs](https://ffmpeg.org/ffmpeg-codecs.html) to learn more.
+        ffmpeg_options: Option<HashMap<Arc<str>, Arc<str>>>,
+    },
+    #[serde(rename = "vulkan_h264")]
+    VulkanH264 {
+        /// Encoding bitrate. If not provided, bitrate is calculated based on resolution and framerate.
+        /// For example at 1080p 30 FPS the average bitrate is 5000 kbit/s and max bitrate is 6250 kbit/s.
+        bitrate: Option<VulkanH264EncoderBitrate>,
+    },
+}
+#[derive(Debug, Serialize, Deserialize, Clone, JsonSchema)]
+#[serde(deny_unknown_fields)]
 pub struct OutputRtpAudioOptions {
     /// (**default="sum_clip"**) Specifies how audio should be mixed.
     pub mixing_strategy: Option<AudioMixingStrategy>,
-    /// Condition for termination of output stream based on the input streams states.
+    /// Condition for termination of output stream based on the input streams states. If output includes both audio and video streams, then EOS needs to be sent for every type.
     pub send_eos_when: Option<OutputEndCondition>,
     /// Audio encoder options.
     pub encoder: RtpAudioEncoderOptions,
-    /// Specifies channels configuration.
+    /// Channels configuration.
     pub channels: Option<AudioChannels>,
     /// Initial audio mixer configuration for output.
     pub initial: AudioScene,
@@ -41,7 +87,7 @@ pub struct OutputRtpAudioOptions {
 #[serde(tag = "type", rename_all = "snake_case", deny_unknown_fields)]
 pub enum RtpAudioEncoderOptions {
     Opus {
-        /// (**default="voip"**) Specifies preset for audio output encoder.
+        /// (**default="voip"**) Audio output encoder preset.
         preset: Option<OpusEncoderPreset>,
 
         /// (**default=`48000`**) Sample rate. Allowed values: [8000, 16000, 24000, 48000].
--- smelter-api/src/output/rtp_into.rs
+++ smelter-api/src/output/rtp_into.rs
@@ -19,7 +19,23 @@ impl TryFrom<RtpOutput> for core::RegisterOutputOptions {
             ));
         }
 
-        let (video_encoder_options, output_video_options) = maybe_video_options(video)?;
+        let (video_encoder_options, output_video_options) = match video {
+            Some(OutputRtpVideoOptions {
+                resolution,
+                send_eos_when,
+                encoder,
+                initial,
+            }) => {
+                let encoder_options = encoder.to_pipeline_options(resolution)?;
+                let output_options = core::RegisterOutputVideoOptions {
+                    initial: initial.try_into()?,
+                    end_condition: send_eos_when.unwrap_or_default().try_into()?,
+                };
+                (Some(encoder_options), Some(output_options))
+            }
+            None => (None, None),
+        };
+
         let (audio_encoder_options, output_audio_options) = match audio {
             Some(OutputRtpAudioOptions {
                 mixing_strategy,
@@ -28,47 +44,18 @@ impl TryFrom<RtpOutput> for core::RegisterOutputOptions {
                 channels,
                 initial,
             }) => {
-                let (audio_encoder_options, resolved_channels) = match encoder {
-                    RtpAudioEncoderOptions::Opus {
-                        preset,
-                        sample_rate,
-                        forward_error_correction,
-                        expected_packet_loss,
-                    } => {
-                        let resolved_channels = channels.unwrap_or(AudioChannels::Stereo);
-
-                        let packet_loss = match expected_packet_loss {
-                            Some(x) if x > 100 => {
-                                return Err(TypeError::new(
-                                    "Expected packet loss value must be from [0, 100] range.",
-                                ));
-                            }
-                            Some(x) => x as i32,
-                            None => 0,
-                        };
-
-                        (
-                            core::AudioEncoderOptions::Opus(core::OpusEncoderOptions {
-                                channels: resolved_channels.into(),
-                                preset: preset.unwrap_or(OpusEncoderPreset::Voip).into(),
-                                sample_rate: sample_rate.unwrap_or(48000),
-                                forward_error_correction: forward_error_correction.unwrap_or(false),
-                                packet_loss,
-                            }),
-                            resolved_channels,
-                        )
-                    }
-                };
-                let output_audio_options = core::RegisterOutputAudioOptions {
+                let channels = channels.unwrap_or(AudioChannels::Stereo);
+                let encoder_options = encoder.to_pipeline_options(channels)?;
+                let output_options = core::RegisterOutputAudioOptions {
                     initial: initial.try_into()?,
                     end_condition: send_eos_when.unwrap_or_default().try_into()?,
                     mixing_strategy: mixing_strategy
                         .unwrap_or(AudioMixingStrategy::SumClip)
                         .into(),
-                    channels: resolved_channels.into(),
+                    channels: channels.into(),
                 };
 
-                (Some(audio_encoder_options), Some(output_audio_options))
+                (Some(encoder_options), Some(output_options))
             }
             None => (None, None),
         };
@@ -117,61 +104,89 @@ impl TryFrom<RtpOutput> for core::RegisterOutputOptions {
     }
 }
 
-fn maybe_video_options(
-    options: Option<OutputVideoOptions>,
-) -> Result<
-    (
-        Option<core::VideoEncoderOptions>,
-        Option<core::RegisterOutputVideoOptions>,
-    ),
-    TypeError,
-> {
-    let Some(options) = options else {
-        return Ok((None, None));
-    };
+impl RtpVideoEncoderOptions {
+    fn to_pipeline_options(
+        &self,
+        resolution: Resolution,
+    ) -> Result<core::VideoEncoderOptions, TypeError> {
+        let encoder_options = match self {
+            RtpVideoEncoderOptions::FfmpegH264 {
+                preset,
+                pixel_format,
+                ffmpeg_options,
+            } => core::VideoEncoderOptions::FfmpegH264(core::FfmpegH264EncoderOptions {
+                preset: preset.unwrap_or(H264EncoderPreset::Fast).into(),
+                resolution: resolution.into(),
+                pixel_format: pixel_format.unwrap_or(PixelFormat::Yuv420p).into(),
+                raw_options: ffmpeg_options
+                    .clone()
+                    .unwrap_or_default()
+                    .into_iter()
+                    .collect(),
+            }),
+            RtpVideoEncoderOptions::VulkanH264 { bitrate } => {
+                core::VideoEncoderOptions::VulkanH264(core::VulkanH264EncoderOptions {
+                    resolution: resolution.into(),
+                    bitrate: bitrate.map(|bitrate| bitrate.try_into()).transpose()?,
+                })
+            }
+            RtpVideoEncoderOptions::FfmpegVp8 { ffmpeg_options } => {
+                core::VideoEncoderOptions::FfmpegVp8(core::FfmpegVp8EncoderOptions {
+                    resolution: resolution.into(),
+                    raw_options: ffmpeg_options
+                        .clone()
+                        .unwrap_or_default()
+                        .into_iter()
+                        .collect(),
+                })
+            }
+            RtpVideoEncoderOptions::FfmpegVp9 {
+                pixel_format,
+                ffmpeg_options,
+            } => core::VideoEncoderOptions::FfmpegVp9(core::FfmpegVp9EncoderOptions {
+                resolution: resolution.into(),
+                pixel_format: pixel_format.unwrap_or(PixelFormat::Yuv420p).into(),
+                raw_options: ffmpeg_options
+                    .clone()
+                    .unwrap_or_default()
+                    .into_iter()
+                    .collect(),
+            }),
+        };
+        Ok(encoder_options)
+    }
+}
 
-    let encoder_options = match options.encoder {
-        VideoEncoderOptions::FfmpegH264 {
-            preset,
-            pixel_format,
-            ffmpeg_options,
-        } => core::VideoEncoderOptions::FfmpegH264(core::FfmpegH264EncoderOptions {
-            preset: preset.unwrap_or(H264EncoderPreset::Fast).into(),
-            resolution: options.resolution.into(),
-            pixel_format: pixel_format.unwrap_or(PixelFormat::Yuv420p).into(),
-            raw_options: ffmpeg_options.unwrap_or_default().into_iter().collect(),
-        }),
-        #[cfg(feature = "vk-video")]
-        VideoEncoderOptions::VulkanH264 { bitrate } => {
-            core::VideoEncoderOptions::VulkanH264(core::VulkanH264EncoderOptions {
-                resolution: options.resolution.into(),
-                bitrate: bitrate.map(|bitrate| bitrate.try_into()).transpose()?,
-            })
-        }
-        #[cfg(not(feature = "vk-video"))]
-        VideoEncoderOptions::VulkanH264 { .. } => {
-            return Err(TypeError::new(super::NO_VULKAN_VIDEO));
-        }
-        VideoEncoderOptions::FfmpegVp8 { ffmpeg_options } => {
-            core::VideoEncoderOptions::FfmpegVp8(core::FfmpegVp8EncoderOptions {
-                resolution: options.resolution.into(),
-                raw_options: ffmpeg_options.unwrap_or_default().into_iter().collect(),
-            })
-        }
-        VideoEncoderOptions::FfmpegVp9 {
-            pixel_format,
-            ffmpeg_options,
-        } => core::VideoEncoderOptions::FfmpegVp9(core::FfmpegVp9EncoderOptions {
-            resolution: options.resolution.into(),
-            pixel_format: pixel_format.unwrap_or(PixelFormat::Yuv420p).into(),
-            raw_options: ffmpeg_options.unwrap_or_default().into_iter().collect(),
-        }),
-    };
-
-    let output_options = core::RegisterOutputVideoOptions {
-        initial: options.initial.try_into()?,
-        end_condition: options.send_eos_when.unwrap_or_default().try_into()?,
-    };
-
-    Ok((Some(encoder_options), Some(output_options)))
+impl RtpAudioEncoderOptions {
+    fn to_pipeline_options(
+        &self,
+        channels: AudioChannels,
+    ) -> Result<core::AudioEncoderOptions, TypeError> {
+        let audio_encoder_options = match self {
+            RtpAudioEncoderOptions::Opus {
+                preset,
+                sample_rate,
+                forward_error_correction,
+                expected_packet_loss,
+            } => {
+                let packet_loss = match expected_packet_loss {
+                    Some(x) if *x > 100 => {
+                        return Err(TypeError::new(
+                            "Expected packet loss value must be from [0, 100] range.",
+                        ));
+                    }
+                    Some(x) => *x as i32,
+                    None => 0,
+                };
+                core::AudioEncoderOptions::Opus(core::OpusEncoderOptions {
+                    channels: channels.into(),
+                    preset: preset.unwrap_or(OpusEncoderPreset::Voip).into(),
+                    sample_rate: sample_rate.unwrap_or(48000),
+                    forward_error_correction: forward_error_correction.unwrap_or(false),
+                    packet_loss,
+                })
+            }
+        };
+        Ok(audio_encoder_options)
+    }
 }
