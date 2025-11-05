87be4a2f9202ae7dca82ce43a881d349cb3b2d5b
Wojciech Kaźmierczak <wojciech.kazmierczak@swmansion.com>
2025-11-05 14:11:39 +0100

    do not overide logger level

--- smelter-core/src/pipeline/rtp/depayloader/static_stream.rs
+++ smelter-core/src/pipeline/rtp/depayloader/static_stream.rs
@@ -1,5 +1,5 @@
 use smelter_render::error::ErrorStack;
-use tracing::warn;
+use tracing::debug;
 
 use crate::pipeline::rtp::{
     RtpPacket,
@@ -41,7 +41,7 @@ where
             Some(PipelineEvent::Data(packet)) => match self.depayloader.depayload(packet) {
                 Ok(chunks) => Some(chunks.into_iter().map(PipelineEvent::Data).collect()),
                 Err(err) => {
-                    warn!("Depayloader error: {}", ErrorStack::new(&err).into_string());
+                    debug!("Depayloader error: {}", ErrorStack::new(&err).into_string());
                     Some(vec![])
                 }
             },
