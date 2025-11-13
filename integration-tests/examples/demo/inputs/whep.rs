ffd536e73d86868d5f94e14a4dcd630c045be33b
Jakub Rękas <74935888+JBRS307@users.noreply.github.com>
2025-11-13 15:48:43 +0100

    [examples] Added Fishjam integration to `WHEP` input. (#1517)
    Co-authored-by: Wojciech Kozyra <wojciech.kozyra@swmansion.com>

--- integration-tests/examples/demo/inputs/whep.rs
+++ integration-tests/examples/demo/inputs/whep.rs
@@ -6,12 +6,21 @@ use rand::RngCore;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use strum::{Display, EnumIter, IntoEnumIterator};
-use tracing::info;
 
 use crate::inputs::VideoDecoder;
 
 const WHEP_TOKEN_ENV: &str = "WHEP_INPUT_BEARER_TOKEN";
 const WHEP_URL_ENV: &str = "WHEP_INPUT_URL";
+
+#[derive(Debug, Display, EnumIter, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum WhepInputPlayer {
+    #[strum(to_string = "Fishjam")]
+    Fishjam,
+
+    #[strum(to_string = "Manual")]
+    Manual,
+}
 
 #[derive(Debug, Display, EnumIter, Clone)]
 pub enum WhepRegisterOptions {
@@ -34,6 +43,7 @@ pub struct WhepInputOptions {
     endpoint_url: String,
     bearer_token: String,
     video: Option<WhepInputVideoOptions>,
+    player: WhepInputPlayer,
 }
 
 impl From<WhepInputOptions> for WhepInput {
@@ -63,6 +73,7 @@ impl WhepInput {
             endpoint_url,
             bearer_token,
             video,
+            ..
         } = &self.options;
         json!({
             "type": "whep_client",
@@ -73,25 +84,30 @@ impl WhepInput {
     }
 
     pub fn on_before_registration(&mut self) -> Result<()> {
-        let cmd = "docker run -e UDP_MUX_PORT=8080 -e NAT_1_TO_1_IP=127.0.0.1 -e NETWORK_TEST_ON_START=false -p 8080:8080 -p 8080:8080/udp seaduboi/broadcast-box";
-        let url = "http://127.0.0.1:8080";
+        match self.options.player {
+            WhepInputPlayer::Manual => {
+                let cmd = "docker run -e UDP_MUX_PORT=8080 -e NAT_1_TO_1_IP=127.0.0.1 -e NETWORK_TEST_ON_START=false -p 8080:8080 -p 8080:8080/udp seaduboi/broadcast-box";
+                let url = "http://127.0.0.1:8080";
 
-        println!("Instructions to start streaming:");
-        println!("1. Start Broadcast Box: {cmd}");
-        println!("2. Open: {url}");
-        println!("3. Make sure that 'I want to stream' option is selected.");
-        println!(
-            "4. Enter '{}' in 'Stream Key' field",
-            self.options.bearer_token,
-        );
+                println!("Instructions to start streaming:");
+                println!("1. Start Broadcast Box: {cmd}");
+                println!("2. Open: {url}");
+                println!("3. Make sure that 'I want to stream' option is selected.");
+                println!(
+                    "4. Enter '{}' in 'Stream Key' field",
+                    self.options.bearer_token,
+                );
 
-        loop {
-            let confirmation = Confirm::new("Is server running? [Y/n]")
-                .with_default(true)
-                .prompt()?;
-            if confirmation {
-                return Ok(());
+                loop {
+                    let confirmation = Confirm::new("Is server running? [Y/n]")
+                        .with_default(true)
+                        .prompt()?;
+                    if confirmation {
+                        return Ok(());
+                    }
+                }
             }
+            WhepInputPlayer::Fishjam => Ok(()),
         }
     }
 }
@@ -101,6 +117,7 @@ pub struct WhepInputBuilder {
     endpoint_url: Option<String>,
     bearer_token: String,
     video: Option<WhepInputVideoOptions>,
+    player: WhepInputPlayer,
 }
 
 impl WhepInputBuilder {
@@ -112,37 +129,74 @@ impl WhepInputBuilder {
             endpoint_url: None,
             bearer_token: "example".to_string(),
             video: None,
+            player: WhepInputPlayer::Manual,
         }
     }
 
     pub fn prompt(self) -> Result<Self> {
-        self.prompt_url()?.prompt_bearer_token()?.prompt_video()
+        self.prompt_video()?
+            .prompt_player()?
+            .prompt_url()?
+            .prompt_bearer_token()
     }
 
-    pub fn prompt_url(self) -> Result<Self> {
-        const BROADCAST_BOX_URL: &str = "http://127.0.0.1:8080/api/whep";
-        let env_url = env::var(WHEP_URL_ENV).unwrap_or_default();
-        let endpoint_url_input = Text::new("Enter the WHEP endpoint URL (ESC for BroadcastBox):")
-            .with_initial_value(&env_url)
-            .prompt_skippable()?;
-
-        match endpoint_url_input {
-            Some(url) if !url.trim().is_empty() => Ok(self.with_endpoint_url(url)),
-            Some(_) | None => Ok(self.with_endpoint_url(BROADCAST_BOX_URL.to_string())),
+    fn prompt_player(self) -> Result<Self> {
+        let player_options = WhepInputPlayer::iter().collect();
+        let player_selection =
+            Select::new("Select input player (ESC for Manual): ", player_options)
+                .prompt_skippable()?;
+        match player_selection {
+            Some(player) => Ok(self.with_player(player)),
+            None => Ok(self),
         }
     }
 
-    // It doesn't actually prompt, but is used in chain
+    fn prompt_url(self) -> Result<Self> {
+        match self.player {
+            WhepInputPlayer::Manual => {
+                const BROADCAST_BOX_URL: &str = "http://127.0.0.1:8080/api/whep";
+                let env_url = env::var(WHEP_URL_ENV).unwrap_or_default();
+                let endpoint_url_input =
+                    Text::new("Enter the WHEP endpoint URL (ESC for BroadcastBox):")
+                        .with_initial_value(&env_url)
+                        .prompt_skippable()?;
+
+                match endpoint_url_input {
+                    Some(url) if !url.trim().is_empty() => Ok(self.with_endpoint_url(url)),
+                    Some(_) | None => Ok(self.with_endpoint_url(BROADCAST_BOX_URL.to_string())),
+                }
+            }
+            WhepInputPlayer::Fishjam => {
+                const FISHJAM_URL: &str = "https://fishjam.io/api/v1/live/api/whep";
+                Ok(self.with_endpoint_url(FISHJAM_URL.to_string()))
+            }
+        }
+    }
+
     fn prompt_bearer_token(self) -> Result<Self> {
-        match env::var(WHEP_TOKEN_ENV).ok() {
-            Some(token) => {
-                info!("WHEP bearer token read from env: {token}");
-                Ok(self.with_bearer_token(token))
-            }
-            None => {
-                info!("Using default WHEP bearer token '{}'", self.bearer_token);
-                Ok(self)
-            }
+        let env_token = env::var(WHEP_TOKEN_ENV).unwrap_or_default();
+        if self.player == WhepInputPlayer::Fishjam {
+            println!();
+            println!(
+                "1. Visit https://fishjam.io and sign in. Create an account if you don't have one."
+            );
+            println!("2. Copy your Fishjam ID from dashboard.");
+            println!("3. Visit https://livestreaming.fishjam.io/");
+            println!("4. Open network tab in dev tools and reload if necessary.");
+            println!("5. Paste copied Fishjam ID in the appropriate input field.");
+            println!("6. Start streaming and press \"Connect to stream\".");
+            println!("7. In dev tools search for the request named \"whep\" using POST method.");
+            println!(
+                "8. In \"Request headers\" section find \"Authorization\" header and copy a token from there."
+            );
+            println!("9. Disconnect from watching the stream before pasting the token.");
+        }
+        let token_input = Text::new("Enter the WHEP bearer token. (ESC for \"example\"):")
+            .with_initial_value(&env_token)
+            .prompt_skippable()?;
+        match token_input {
+            Some(token) if !token.trim().is_empty() => Ok(self.with_bearer_token(token)),
+            Some(_) | None => Ok(self.with_bearer_token("example".to_string())),
         }
     }
 
@@ -197,11 +251,17 @@ impl WhepInputBuilder {
         self
     }
 
+    pub fn with_player(mut self, player: WhepInputPlayer) -> Self {
+        self.player = player;
+        self
+    }
+
     pub fn build(self) -> WhepInput {
         let options = WhepInputOptions {
             endpoint_url: self.endpoint_url.unwrap(),
             bearer_token: self.bearer_token,
             video: self.video,
+            player: self.player,
         };
         WhepInput {
             name: self.name,
