e8b03d8765161f0cad66a050208fed8036596e72
Wojciech Kozyra <wkozyra95@gmail.com>
2025-11-27 20:15:44 +0100

    [ts-sdk] Release `0.3.0-rc.1`

--- ts/smelter-node/src/manager/locallySpawnedInstance.ts
+++ ts/smelter-node/src/manager/locallySpawnedInstance.ts
@@ -20,7 +20,7 @@ import { smelterInstanceLoggerOptions } from '../logger';
 import { getSmelterStatus } from '../getSmelterStatus';
 
 // TODO: This should be changed to `software-mansion/smelter` repo with proper version on ts-sdk release
-const VERSION = `6e118f3f`;
+const VERSION = `62d73800`;
 const REPO = `smelter-labs/smelter-rc`;
 
 // const VERSION = `v0.4.1`;
