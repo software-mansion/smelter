51b879731ab6b08e96069888d552d3d6e1d7ff9e
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2025-12-02 13:03:30 +0100

    update url

--- ts/smelter-node/src/manager/locallySpawnedInstance.ts
+++ ts/smelter-node/src/manager/locallySpawnedInstance.ts
@@ -20,11 +20,11 @@ import { smelterInstanceLoggerOptions } from '../logger';
 import { getSmelterStatus } from '../getSmelterStatus';
 
 // TODO: This should be changed to `software-mansion/smelter` repo with proper version on ts-sdk release
-const VERSION = `62d73800`;
-const REPO = `smelter-labs/smelter-rc`;
+//const VERSION = `62d73800`;
+//const REPO = `smelter-labs/smelter-rc`;
 
-// const VERSION = `v0.4.1`;
-// const REPO = `software-mansion/smelter`;
+const VERSION = `v0.5.0`;
+const REPO = `software-mansion/smelter`;
 
 export type LocallySpawnedInstanceOptions = {
   port: number;
