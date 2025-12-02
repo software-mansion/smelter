869cb25c83b5a7a4707686235549fb9de388370e
Jakub Rękas <jakub.rekas@swmansion.com>
2025-12-02 12:13:35 +0100

    Removed custom ffmpeg logger level

--- src/logger.rs
+++ src/logger.rs
@@ -105,7 +105,5 @@ pub fn init_logger(opts: LoggerConfig) {
 
     unsafe {
         ffmpeg_next::sys::av_log_set_callback(Some(ffmpeg_log_callback));
-        // TODO: #remove
-        ffmpeg_next::sys::av_log_set_level(100);
     }
 }
