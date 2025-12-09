ed9a1b3e7a27090ec1a6b9323e9dd810c38fe261
Jakub Rękas <jakub.rekas@swmansion.com>
2025-12-09 12:15:34 +0100

    Removed unnecessary nonzeros

--- smelter-core/src/pipeline/encoder/utils.rs
+++ smelter-core/src/pipeline/encoder/utils.rs
@@ -1,5 +1,3 @@
-use std::num::NonZero;
-
 use smelter_render::{Framerate, Resolution};
 
 use crate::codecs::VideoEncoderBitrate;
@@ -10,11 +8,11 @@ pub(super) fn bitrate_from_resolution_framerate(
 ) -> VideoEncoderBitrate {
     const PRECISION: f64 = 500_000.0; // 500kb
     const BPP: f64 = 0.08;
-    let width = NonZero::new(u32::max(resolution.width as u32, 1)).unwrap();
-    let height = NonZero::new(u32::max(resolution.height as u32, 1)).unwrap();
+    let width = u32::max(resolution.width as u32, 1);
+    let height = u32::max(resolution.height as u32, 1);
 
     let average_bitrate =
-        (width.get() * height.get()) as f64 * (framerate.num as f64 / framerate.den as f64) * BPP;
+        (width * height) as f64 * (framerate.num as f64 / framerate.den as f64) * BPP;
     let average_bitrate = (average_bitrate / PRECISION).ceil() * PRECISION;
     let max_bitrate = average_bitrate * 1.25;
 
