e68a556ee483267e5875f8dc6f3f2e150e3bac0b
Jakub Rękas <jakub.rekas@swmansion.com>
2025-12-22 16:25:33 +0100

    Changed handling of avc and aac config

--- rtmp/flv/src/parser/mod.rs
+++ rtmp/flv/src/parser/mod.rs
@@ -2,7 +2,7 @@ use std::mem;
 
 use bytes::Bytes;
 
-use crate::{AudioTag, ParseError, VideoTag};
+use crate::{AudioTag, ParseError, VideoTag, tag::PacketType};
 
 pub mod audio;
 pub mod error;
@@ -10,6 +10,9 @@ pub mod video;
 
 #[derive(Debug, Default)]
 pub struct RtmpParser {
+    avc_video_config: Option<VideoTag>,
+    aac_audio_config: Option<AudioTag>,
+
     video: Vec<VideoTag>,
     audio: Vec<AudioTag>,
 }
@@ -19,18 +22,32 @@ impl RtmpParser {
         Self::default()
     }
 
-    pub fn parse_video(&mut self, data: Bytes) -> Result<(), ParseError> {
+    pub fn parse_video(&mut self, data: Bytes) -> Result<bool, ParseError> {
         let video_tag = VideoTag::parse(data)?;
-
-        self.video.push(video_tag);
-        Ok(())
+        match video_tag.packet_type {
+            PacketType::Data => {
+                self.video.push(video_tag);
+                Ok(false)
+            }
+            PacketType::Config => {
+                self.avc_video_config = Some(video_tag);
+                Ok(true)
+            }
+        }
     }
 
-    pub fn parse_audio(&mut self, data: Bytes) -> Result<(), ParseError> {
+    pub fn parse_audio(&mut self, data: Bytes) -> Result<bool, ParseError> {
         let audio_tag = AudioTag::parse(data)?;
-
-        self.audio.push(audio_tag);
-        Ok(())
+        match audio_tag.packet_type {
+            PacketType::Data => {
+                self.audio.push(audio_tag);
+                Ok(false)
+            }
+            PacketType::Config => {
+                self.aac_audio_config = Some(audio_tag);
+                Ok(true)
+            }
+        }
     }
 
     pub fn video(&mut self) -> Vec<VideoTag> {
