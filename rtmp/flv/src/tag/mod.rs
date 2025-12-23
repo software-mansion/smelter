114b4035148700e2dea92251bb5f4d103ef822b0
Jakub Rękas <jakub.rekas@swmansion.com>
2025-12-23 10:49:33 +0100

    Added doc comments

--- rtmp/flv/src/tag/mod.rs
+++ rtmp/flv/src/tag/mod.rs
@@ -1,8 +1,11 @@
 pub mod audio;
 pub mod video;
 
+/// Information if tag contains av data or decoder config.
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum PacketType {
     Data,
+
+    /// This field is valid only for AVC for video and AAC for audio.
     Config,
 }
