806449e894c2f59883eabd7d172de8cbf61a86a0
Jakub Rękas <jakub.rekas@swmansion.com>
2026-01-29 14:55:45 +0100

    WIP: Remodeled decoding of complex types, implementing

--- rtmp/flv/src/amf3/decoding.rs
+++ rtmp/flv/src/amf3/decoding.rs
@@ -115,19 +115,19 @@ impl Decoder {
         Ok(amf_value)
     }
 
-    fn decode_integer(&mut self, buf: &mut Bytes) -> Result<i32, DecodingError> {
+    fn decode_integer(&mut self, buf: &mut Bytes) -> Result<AmfValue, DecodingError> {
         todo!()
     }
 
-    fn decode_double(&mut self, buf: &mut Bytes) -> Result<f64, DecodingError> {
+    fn decode_double(&mut self, buf: &mut Bytes) -> Result<AmfValue, DecodingError> {
         if buf.remaining() < 8 {
             return Err(DecodingError::InsufficientData);
         }
 
-        Ok(buf.get_f64())
+        Ok(AmfValue::Double(buf.get_f64()))
     }
 
-    fn decode_string(&mut self, buf: &mut Bytes) -> Result<String, DecodingError> {
+    fn decode_string(&mut self, buf: &mut Bytes) -> Result<AmfValue, DecodingError> {
         if buf.remaining() < 4 {
             return Err(DecodingError::InsufficientData);
         }
@@ -157,75 +157,39 @@ impl Decoder {
             }
         };
 
-        Ok(string)
+        Ok(AmfValue::String(string))
     }
 
-    fn decode_xml_doc(&mut self, buf: &mut Bytes) -> Result<String, DecodingError> {
-        if buf.remaining() < 4 {
-            return Err(DecodingError::InsufficientData);
-        }
-
-        let u29 = decode_u29(buf)?;
-        let has_value = (u29 & 0b1) == 1;
-        let u28 = u29 >> 1;
-
-        let xml = match has_value {
-            true => {
-                let size = u28 as usize;
-                if buf.remaining() < size {
-                    return Err(DecodingError::InsufficientData);
-                }
-
-                let utf8 = buf.copy_to_bytes(size).to_vec();
-                let xml = String::from_utf8(utf8).map_err(|_| DecodingError::InvalidUtf8)?;
-                self.complexes.push(AmfValue::XmlDoc(xml.clone()));
-                xml
+    fn decode_xml_doc(&mut self, buf: &mut Bytes) -> Result<AmfValue, DecodingError> {
+        let decode = |decoder: &mut Decoder, buf: &mut Bytes, size: usize| {
+            if buf.remaining() < size {
+                return Err(DecodingError::InsufficientData);
             }
-            false => {
-                let idx = u28 as usize;
-                let ref_value = self
-                    .complexes
-                    .get(idx)
-                    .ok_or(DecodingError::OutOfBoundsReference)?;
 
-                match ref_value {
-                    AmfValue::XmlDoc(xml) => xml.clone(),
-                    _ => return Err(DecodingError::InvalidReferenceType),
-                }
-            }
+            let utf8 = buf.split_to(size);
+            let xml = String::from_utf8(utf8.to_vec()).map_err(|_| DecodingError::InvalidUtf8)?;
+
+            let amf_value = AmfValue::XmlDoc(xml);
+            decoder.complexes.push(amf_value.clone());
+            Ok(amf_value)
         };
-        Ok(xml)
+
+        self.decode_complex(buf, decode)
     }
 
-    fn decode_date(&mut self, buf: &mut Bytes) -> Result<f64, DecodingError> {
-        if buf.remaining() < 4 {
-            return Err(DecodingError::InsufficientData);
-        }
-
-        let u29 = decode_u29(buf)?;
-        let has_value = (u29 & 0b1) == 1;
-        let u28 = u29 >> 1;
-
-        let date = match has_value {
-            true => {
-                let date = self.decode_double(buf)?;
-                self.complexes.push(AmfValue::Date(date));
-                date
+    fn decode_date(&mut self, buf: &mut Bytes) -> Result<AmfValue, DecodingError> {
+        let decode = |decoder: &mut Decoder, buf: &mut Bytes, _| {
+            if buf.remaining() < 8 {
+                return Err(DecodingError::InsufficientData);
             }
-            false => {
-                let idx = u28 as usize;
-                let ref_value = self
-                    .complexes
-                    .get(idx)
-                    .ok_or(DecodingError::OutOfBoundsReference)?;
 
-                match ref_value {
-                    AmfValue::Date(date) => *date,
-                    _ => return Err(DecodingError::InvalidReferenceType),
-                }
-            }
+            let double_date = buf.get_f64();
+            let amf_value = AmfValue::Date(double_date);
+            decoder.complexes.push(amf_value.clone());
+            Ok(amf_value)
         };
-        Ok(date)
+
+        self.decode_complex(buf, decode)
     }
 
     fn decode_array(&mut self, buf: &mut Bytes) -> Result<Array, DecodingError> {
@@ -237,196 +201,19 @@ impl Decoder {
     }
 
     fn decode_xml(&mut self, buf: &mut Bytes) -> Result<String, DecodingError> {
-        if buf.remaining() < 4 {
-            return Err(DecodingError::InsufficientData);
-        }
-
-        let u29 = decode_u29(buf)?;
-        let has_value = (u29 & 0b1) == 1;
-        let u28 = u29 >> 1;
-
-        let xml = match has_value {
-            true => {
-                let size = u28 as usize;
-                if buf.remaining() < size {
-                    return Err(DecodingError::InsufficientData);
-                }
-
-                let utf8 = buf.copy_to_bytes(size).to_vec();
-                let xml = String::from_utf8(utf8).map_err(|_| DecodingError::InvalidUtf8)?;
-                self.complexes.push(AmfValue::XmlDoc(xml.clone()));
-                xml
-            }
-            false => {
-                let idx = u28 as usize;
-                let ref_value = self
-                    .complexes
-                    .get(idx)
-                    .ok_or(DecodingError::OutOfBoundsReference)?;
-
-                match ref_value {
-                    AmfValue::XmlDoc(xml) => xml.clone(),
-                    _ => return Err(DecodingError::InvalidReferenceType),
-                }
-            }
-        };
-        Ok(xml)
+        todo!()
     }
 
     fn decode_byte_array(&mut self, buf: &mut Bytes) -> Result<Bytes, DecodingError> {
-        if buf.remaining() < 4 {
-            return Err(DecodingError::InsufficientData);
-        }
-
-        let u29 = decode_u29(buf)?;
-        let has_value = (u29 & 0b1) == 1;
-        let u28 = u29 >> 1;
-
-        let bytes = match has_value {
-            true => {
-                let size = u28 as usize;
-                if buf.remaining() < size {
-                    return Err(DecodingError::InsufficientData);
-                }
-
-                let bytes = buf.copy_to_bytes(size);
-                self.complexes.push(AmfValue::ByteArray(bytes.clone()));
-                bytes
-            }
-            false => {
-                let idx = u28 as usize;
-                let ref_value = self
-                    .complexes
-                    .get(idx)
-                    .ok_or(DecodingError::OutOfBoundsReference)?;
-
-                match ref_value {
-                    AmfValue::ByteArray(bytes) => bytes.clone(),
-                    _ => return Err(DecodingError::InvalidReferenceType),
-                }
-            }
-        };
-        Ok(bytes)
+        todo!()
     }
 
     fn decode_int_vec(&mut self, buf: &mut Bytes) -> Result<VectorInt, DecodingError> {
-        const ITEM_SIZE: usize = 4;
-
-        if buf.remaining() < 4 {
-            return Err(DecodingError::InsufficientData);
-        }
-
-        let u29 = decode_u29(buf)?;
-        let has_value = (u29 & 0b1) == 1;
-        let u28 = u29 >> 1;
-
-        let vec = match has_value {
-            true => {
-                let items = u28 as usize;
-                if buf.remaining() < items * ITEM_SIZE + 1 {
-                    return Err(DecodingError::InsufficientData);
-                }
-
-                let fixed_length = buf.get_u8() != 0x00;
-
-                let mut vec_bytes = buf.split_to(items * ITEM_SIZE);
-                let mut values = Vec::with_capacity(items * ITEM_SIZE);
-
-                while vec_bytes.has_remaining() {
-                    let int = self.decode_integer(&mut vec_bytes)?;
-                    values.push(int);
-                }
-
-                self.complexes.push(AmfValue::VectorInt {
-                    fixed_length,
-                    values: values.clone(),
-                });
-
-                VectorInt {
-                    fixed_length,
-                    values,
-                }
-            }
-            false => {
-                let idx = u28 as usize;
-                let ref_value = self
-                    .complexes
-                    .get(idx)
-                    .ok_or(DecodingError::OutOfBoundsReference)?;
-
-                match ref_value {
-                    AmfValue::VectorInt {
-                        fixed_length,
-                        values,
-                    } => VectorInt {
-                        fixed_length: *fixed_length,
-                        values: values.clone(),
-                    },
-                    _ => return Err(DecodingError::InvalidReferenceType),
-                }
-            }
-        };
-        Ok(vec)
+        todo!()
     }
 
     fn decode_uint_vec(&mut self, buf: &mut Bytes) -> Result<VectorUInt, DecodingError> {
-        const ITEM_SIZE: usize = 4;
-
-        if buf.remaining() < 4 {
-            return Err(DecodingError::InsufficientData);
-        }
-
-        let u29 = decode_u29(buf)?;
-        let has_value = (u29 & 0b1) == 1;
-        let u28 = u29 >> 1;
-
-        let vec = match has_value {
-            true => {
-                let items = u28 as usize;
-                if buf.remaining() < items * ITEM_SIZE + 1 {
-                    return Err(DecodingError::InsufficientData);
-                }
-
-                let fixed_length = buf.get_u8() != 0x00;
-
-                let mut vec_bytes = buf.split_to(items * ITEM_SIZE);
-                let mut values = Vec::with_capacity(items * ITEM_SIZE);
-
-                while vec_bytes.has_remaining() {
-                    let uint = decode_u29(&mut vec_bytes)?;
-                    values.push(uint);
-                }
-
-                self.complexes.push(AmfValue::VectorUInt {
-                    fixed_length,
-                    values: values.clone(),
-                });
-
-                VectorUInt {
-                    fixed_length,
-                    values,
-                }
-            }
-            false => {
-                let idx = u28 as usize;
-                let ref_value = self
-                    .complexes
-                    .get(idx)
-                    .ok_or(DecodingError::OutOfBoundsReference)?;
-
-                match ref_value {
-                    AmfValue::VectorUInt {
-                        fixed_length,
-                        values,
-                    } => VectorUInt {
-                        fixed_length: *fixed_length,
-                        values: values.clone(),
-                    },
-                    _ => return Err(DecodingError::InvalidReferenceType),
-                }
-            }
-        };
-        Ok(vec)
+        todo!()
     }
 
     fn decode_double_vec(&mut self, buf: &mut Bytes) -> Result<VectorDouble, DecodingError> {
@@ -439,6 +226,35 @@ impl Decoder {
 
     fn decode_dictionary(&mut self, buf: &mut Bytes) -> Result<Dictionary, DecodingError> {
         todo!()
+    }
+
+    fn decode_complex<F>(&mut self, buf: &mut Bytes, decode: F) -> Result<AmfValue, DecodingError>
+    where
+        F: FnOnce(&mut Self, &mut Bytes, usize) -> Result<AmfValue, DecodingError>,
+    {
+        if buf.remaining() < 4 {
+            return Err(DecodingError::InsufficientData);
+        }
+
+        let u29 = decode_u29(buf)?;
+        let has_value = (u29 & 0b1) == 1;
+        let u28 = u29 >> 1;
+
+        let amf_value = match has_value {
+            true => {
+                let size = u28 as usize;
+                decode(self, buf, size)?
+            }
+            false => {
+                let idx = u28 as usize;
+                self.complexes
+                    .get(idx)
+                    .ok_or(DecodingError::OutOfBoundsReference)?
+                    .clone()
+            }
+        };
+
+        Ok(amf_value)
     }
 }
 
