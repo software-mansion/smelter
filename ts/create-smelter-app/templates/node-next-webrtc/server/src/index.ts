11449ba56bfb44fa15aee2dc6c10eac47f88b1a8
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2026-01-29 15:58:44 +0100

    Add server implementation

---
+++ ts/create-smelter-app/templates/node-next-webrtc/server/src/index.ts
@@ -0,0 +1,10 @@
+import { SmelterInstance } from './smelter';
+import { routes } from './routes';
+
+async function run() {
+  await SmelterInstance.init();
+
+  await routes.listen({ port: 3000, host: '0.0.0.0' });
+}
+
+void run();
---
+++ ts/create-smelter-app/templates/node-next-webrtc/server/src/routes.ts
@@ -0,0 +1,22 @@
+import Fastify from 'fastify';
+import { Type } from '@sinclair/typebox';
+import type { Static, TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
+
+import { store } from './app/store';
+
+export const routes = Fastify().withTypeProvider<TypeBoxTypeProvider>();
+
+export const UpdateLayoutSchema = Type.Object({
+  showInstructions: Type.Boolean(),
+});
+
+export type UpdateLayoutType = Static<typeof UpdateLayoutSchema>;
+
+routes.post<{ Body: UpdateLayoutType }>(
+  '/layout-update',
+  { schema: { body: UpdateLayoutSchema } },
+  async (req, res) => {
+    store.getState().updateLayout(req.body);
+    res.status(200).send({ status: 'ok' });
+  }
+);
