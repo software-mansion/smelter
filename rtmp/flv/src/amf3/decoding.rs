caba38eae783608cfe7d10b1f5c7b1cf4b372a74
Jakub Rękas <jakub.rekas@swmansion.com>
2026-02-03 10:06:34 +0100

    Added comment explaining test

--- rtmp/flv/src/amf3/decoding.rs
+++ rtmp/flv/src/amf3/decoding.rs
@@ -527,6 +527,9 @@ mod decode_test {
     fn test_decode_i29() {
         let decoder = Decoder::new();
 
+        // https://github.com/q191201771/doc/blob/master/spec-amf-file-format-spec.pdf
+        // Formatting of the test cases is explained in 1.3.1 and 3.6
+
         // 32 in 7 bit U2
         let mut one_byte_pos = Bytes::from(vec![0b0010_0000]);
         let decoded_val = decoder
