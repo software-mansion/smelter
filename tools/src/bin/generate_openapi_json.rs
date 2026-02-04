5795925fee1b50b0c0e18f7212131a8873411659
Jakub Rękas <jakub.rekas@swmansion.com>
2026-02-04 16:51:51 +0100

    Added outputs to openapi generation

--- tools/src/bin/generate_openapi_json.rs
+++ tools/src/bin/generate_openapi_json.rs
@@ -1,10 +1,14 @@
 use std::{fs, path::PathBuf};
 
-use smelter_api::RtpInput;
+use smelter_api::{HlsInput, Mp4Input, RtmpInput, RtpInput, V4l2Input, WhepInput, WhipInput};
+use smelter_api::{HlsOutput, Mp4Output, RtmpOutput, RtpOutput, WhepOutput, WhipOutput};
 use utoipa::OpenApi;
 
 #[derive(OpenApi)]
-#[openapi(components(schemas(RtpInput)))]
+#[openapi(components(schemas(
+    HlsInput, Mp4Input, RtmpInput, RtpInput, V4l2Input, WhepInput, WhipInput, HlsOutput, Mp4Output,
+    RtmpOutput, RtpOutput, WhepOutput, WhipOutput
+)))]
 struct ApiDoc;
 
 const ROOT_DIR: &str = env!("CARGO_MANIFEST_DIR");
@@ -13,6 +17,7 @@ fn main() {
     tracing_subscriber::fmt().init();
 
     let schema_json = ApiDoc::openapi().to_pretty_json().unwrap();
-    let gen_path = PathBuf::from(ROOT_DIR).join("schemas/openapi.json");
+
+    let gen_path = PathBuf::from(ROOT_DIR).join("openapi.json");
     fs::write(&gen_path, schema_json).unwrap();
 }
