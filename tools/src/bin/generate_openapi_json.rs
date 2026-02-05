df0181f0f1f9043ead48c3a47ce19639f631c60a
Jakub Rękas <jakub.rekas@swmansion.com>
2026-02-05 13:51:03 +0100

    Added unregister output to openAPI

--- tools/src/bin/generate_openapi_json.rs
+++ tools/src/bin/generate_openapi_json.rs
@@ -20,6 +20,7 @@ use utoipa::OpenApi;
         smelter::routes::register_request::handle_image,
         smelter::routes::register_request::handle_font,
         smelter::routes::unregister_request::handle_input,
+        smelter::routes::unregister_request::handle_output,
     )
 )]
 struct ApiDoc;
