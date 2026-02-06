c3cd3ccf8b795717dcdbdd2b1b989e699529d9bd
Jakub Rękas <jakub.rekas@swmansion.com>
2026-02-06 15:11:00 +0100

    Removed types that are inferred from generating function

--- tools/src/bin/generate_openapi_json.rs
+++ tools/src/bin/generate_openapi_json.rs
@@ -1,32 +1,22 @@
 use std::{fs, path::PathBuf};
 
-use smelter_api::{
-    DeckLink, HlsInput, Mp4Input, RtmpInput, RtpInput, V4l2Input, WhepInput, WhipInput,
-};
-use smelter_api::{HlsOutput, Mp4Output, RtmpOutput, RtpOutput, WhepOutput, WhipOutput};
 use utoipa::OpenApi;
 
 #[derive(OpenApi)]
-#[openapi(
-    components(schemas(
-        DeckLink, HlsInput, Mp4Input, RtmpInput, RtpInput, V4l2Input, WhepInput, WhipInput,
-        HlsOutput, Mp4Output, RtmpOutput, RtpOutput, WhepOutput, WhipOutput
-    )),
-    paths(
-        smelter::routes::register_request::handle_input,
-        smelter::routes::register_request::handle_output,
-        smelter::routes::register_request::handle_shader,
-        smelter::routes::register_request::handle_web_renderer,
-        smelter::routes::register_request::handle_image,
-        smelter::routes::register_request::handle_font,
-        smelter::routes::unregister_request::handle_input,
-        smelter::routes::unregister_request::handle_output,
-        smelter::routes::unregister_request::handle_shader,
-        smelter::routes::unregister_request::handle_web_renderer,
-        smelter::routes::unregister_request::handle_image,
-        smelter::routes::status::status_handler,
-    )
-)]
+#[openapi(paths(
+    smelter::routes::register_request::handle_input,
+    smelter::routes::register_request::handle_output,
+    smelter::routes::register_request::handle_shader,
+    smelter::routes::register_request::handle_web_renderer,
+    smelter::routes::register_request::handle_image,
+    smelter::routes::register_request::handle_font,
+    smelter::routes::unregister_request::handle_input,
+    smelter::routes::unregister_request::handle_output,
+    smelter::routes::unregister_request::handle_shader,
+    smelter::routes::unregister_request::handle_web_renderer,
+    smelter::routes::unregister_request::handle_image,
+    smelter::routes::status::status_handler,
+))]
 struct ApiDoc;
 
 const ROOT_DIR: &str = env!("CARGO_MANIFEST_DIR");
