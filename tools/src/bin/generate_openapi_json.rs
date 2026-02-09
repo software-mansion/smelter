2462ce60455e17a53af33abf6233c65b06f40b57
Jakub Rękas <jakub.rekas@swmansion.com>
2026-02-09 10:02:34 +0100

    Added `info` to openapi generation

--- tools/src/bin/generate_openapi_json.rs
+++ tools/src/bin/generate_openapi_json.rs
@@ -3,25 +3,36 @@ use std::{fs, path::PathBuf};
 use utoipa::OpenApi;
 
 #[derive(OpenApi)]
-#[openapi(paths(
-    smelter::routes::control_request::handle_start,
-    smelter::routes::control_request::handle_reset,
-    smelter::routes::register_request::handle_input,
-    smelter::routes::register_request::handle_output,
-    smelter::routes::register_request::handle_shader,
-    smelter::routes::register_request::handle_web_renderer,
-    smelter::routes::register_request::handle_image,
-    smelter::routes::register_request::handle_font,
-    smelter::routes::unregister_request::handle_input,
-    smelter::routes::unregister_request::handle_output,
-    smelter::routes::unregister_request::handle_shader,
-    smelter::routes::unregister_request::handle_web_renderer,
-    smelter::routes::unregister_request::handle_image,
-    smelter::routes::update_output::handle_output_update,
-    smelter::routes::update_output::handle_keyframe_request,
-    smelter::routes::status::status_handler,
-    smelter::routes::ws::ws_handler,
-))]
+#[openapi(
+    info(
+        title = "Smelter",
+        description = "Real-time video compositing software",
+        version = "0.5.0",
+        license(
+            name = "",
+            url = "https://github.com/software-mansion/smelter/blob/master/LICENSE",
+        ),
+    ),
+    paths(
+        smelter::routes::control_request::handle_start,
+        smelter::routes::control_request::handle_reset,
+        smelter::routes::register_request::handle_input,
+        smelter::routes::register_request::handle_output,
+        smelter::routes::register_request::handle_shader,
+        smelter::routes::register_request::handle_web_renderer,
+        smelter::routes::register_request::handle_image,
+        smelter::routes::register_request::handle_font,
+        smelter::routes::unregister_request::handle_input,
+        smelter::routes::unregister_request::handle_output,
+        smelter::routes::unregister_request::handle_shader,
+        smelter::routes::unregister_request::handle_web_renderer,
+        smelter::routes::unregister_request::handle_image,
+        smelter::routes::update_output::handle_output_update,
+        smelter::routes::update_output::handle_keyframe_request,
+        smelter::routes::status::status_handler,
+        smelter::routes::ws::ws_handler,
+    )
+)]
 struct ApiDoc;
 
 const ROOT_DIR: &str = env!("CARGO_MANIFEST_DIR");
