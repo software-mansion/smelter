c7866313308790537b2a39698e6aef6f84511c91
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2026-02-12 12:18:55 +0100

    wip

--- smelter-core/src/pipeline/utils/mod.rs
+++ smelter-core/src/pipeline/utils/mod.rs
@@ -1,7 +1,9 @@
 pub(super) mod input_buffer;
 
+mod h264_annexb_to_avcc;
 mod h264_au_splitter;
 mod h264_avcc_to_annexb;
 
+pub(super) use h264_annexb_to_avcc::{annexb_to_avcc, build_avc_decoder_config};
 pub(super) use h264_au_splitter::H264AuSplitter;
 pub(super) use h264_avcc_to_annexb::{H264AvcDecoderConfig, H264AvccToAnnexB};
