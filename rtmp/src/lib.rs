655f8f80449718d003ace68a4e8e86e2a64ecf15
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2026-02-12 12:18:55 +0100

    wip

--- rtmp/src/lib.rs
+++ rtmp/src/lib.rs
@@ -2,6 +2,8 @@ mod amf0;
 mod amf3;
 mod buffered_stream_reader;
 mod chunk;
+mod client;
+mod client_handshake;
 mod error;
 mod flv;
 mod handle_client;
@@ -11,6 +13,7 @@ mod negotiation;
 mod protocol;
 mod server;
 
+pub use client::*;
 pub use error::*;
 pub use flv::*;
 pub use server::*;
