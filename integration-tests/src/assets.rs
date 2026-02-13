cf91b01ff6b562c2280c7876e13200671078b4e6
Jakub Rękas <74935888+JBRS307@users.noreply.github.com>
2026-02-13 10:56:50 +0100

    [integration-tests] Changed asset URLs for test videos. (#1720)

--- integration-tests/src/assets.rs
+++ integration-tests/src/assets.rs
@@ -1,10 +1,8 @@
-pub const BUNNY_H264_URL: &str =
-    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
-pub const BUNNY_H264_PATH: &str = "examples/assets/BigBuckBunny720p24fps597s.mp4";
+pub const BUNNY_H264_URL: &str = "https://github.com/smelter-labs/smelter-snapshot-tests/raw/refs/heads/main/assets/BigBuckBunny720p24fps30s.mp4";
+pub const BUNNY_H264_PATH: &str = "examples/assets/BigBuckBunny720p24fps30s.mp4";
 
-pub const ELEPHANT_H264_URL: &str =
-    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4";
-pub const ELEPHANT_H264_PATH: &str = "examples/assets/ElephantsDream720p24fps654s.mp4";
+pub const ELEPHANT_H264_URL: &str = "https://github.com/smelter-labs/smelter-snapshot-tests/raw/refs/heads/main/assets/ElephantsDream720p24fps60s.mp4";
+pub const ELEPHANT_H264_PATH: &str = "examples/assets/ElephantsDream720p24fps60s.mp4";
 
 pub const OCEAN_H264_URL: &str = "https://filesamples.com/samples/video/mp4/sample_1280x720.mp4";
 pub const OCEAN_H264_PATH: &str = "examples/assets/OceanSample720p24fps28s.mp4";
--- integration-tests/src/pipeline_tests/offline_processing.rs
+++ integration-tests/src/pipeline_tests/offline_processing.rs
@@ -11,10 +11,9 @@ use smelter::config::read_config;
 use tokio_tungstenite::tungstenite;
 use tracing::info;
 
-use crate::{CompositorInstance, pipeline_tests::start_server_msg_listener};
-
-const BUNNY_URL: &str =
-    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+use crate::{
+    CompositorInstance, assets::BUNNY_H264_URL, pipeline_tests::start_server_msg_listener,
+};
 
 #[test]
 pub fn offline_processing() -> Result<()> {
@@ -34,7 +33,7 @@ pub fn offline_processing() -> Result<()> {
         "input/input_1/register",
         json!({
             "type": "mp4",
-            "url": BUNNY_URL,
+            "url": BUNNY_H264_URL,
             "offset_ms": 0,
             "required": true
         }),
@@ -123,7 +122,7 @@ pub fn offline_processing() -> Result<()> {
     if !(1.9..=2.1).contains(&duration) {
         return Err(anyhow!("Invalid duration: {}", duration));
     }
-    if !(930_000..=1_000_000).contains(&bit_rate) {
+    if !(2_450_000..=2_530_000).contains(&bit_rate) {
         return Err(anyhow!("Invalid bit rate: {}", bit_rate));
     }
 
