b2fb0834dbd4f8249319618903e8e6b17a22fe5a
Jakub Rękas <kuba.rekas.307@gmail.com>
2026-03-05 11:25:32 +0100

    Bitrate stats for video and audio in whep output

--- smelter-core/src/stats/output_events.rs
+++ smelter-core/src/stats/output_events.rs
@@ -3,6 +3,12 @@ use smelter_render::OutputId;
 use crate::{OutputProtocolKind, Ref, stats::StatsEvent};
 
 #[derive(Debug, Clone, Copy)]
+pub(crate) enum StatsTrackKind {
+    Video,
+    Audio,
+}
+
+#[derive(Debug, Clone, Copy)]
 pub(crate) enum OutputStatsEvent {
     Whep(WhepOutputStatsEvent),
 }
@@ -35,4 +41,17 @@ pub(crate) enum WhepOutputTrackStatsEvent {
     PacketSent,
     NackReceived,
     ChunkSize(u64),
+}
+
+impl WhepOutputTrackStatsEvent {
+    pub(crate) fn into_event(
+        self,
+        output_ref: &Ref<OutputId>,
+        track_kind: StatsTrackKind,
+    ) -> StatsEvent {
+        match track_kind {
+            StatsTrackKind::Video => WhepOutputStatsEvent::Video(self).into_event(output_ref),
+            StatsTrackKind::Audio => WhepOutputStatsEvent::Audio(self).into_event(output_ref),
+        }
+    }
 }
