3c44655db8456a5d39ba9d1e7d008413f03919d8
Jakub Rękas <kuba.rekas.307@gmail.com>
2026-03-24 11:46:40 +0100

    eslint

--- ts/smelter-web-wasm/src/compositor/api.ts
+++ ts/smelter-web-wasm/src/compositor/api.ts
@@ -10,37 +10,37 @@ export type RegisterShader = Renderers.RegisterShader;
 
 export type RegisterOutput =
   | {
-    type: 'stream';
-    video: {
-      resolution: Api.Resolution;
-    };
-    audio?: boolean;
-  }
+      type: 'stream';
+      video: {
+        resolution: Api.Resolution;
+      };
+      audio?: boolean;
+    }
   | {
-    type: 'canvas';
-    video: {
-      canvas: HTMLCanvasElement;
-      resolution: Api.Resolution;
-    };
-    audio?: boolean;
-  }
+      type: 'canvas';
+      video: {
+        canvas: HTMLCanvasElement;
+        resolution: Api.Resolution;
+      };
+      audio?: boolean;
+    }
   | {
-    type: 'whip_client';
-    /**
-     * WHIP server endpoint.
-     */
-    endpointUrl: string;
-    /**
-     * Token for authenticating communication with the WHIP server.
-     */
-    bearerToken?: string;
-    iceServers?: RTCConfiguration['iceServers'];
-    video: {
-      resolution: Api.Resolution;
-      maxBitrate?: number;
+      type: 'whip_client';
+      /**
+       * WHIP server endpoint.
+       */
+      endpointUrl: string;
+      /**
+       * Token for authenticating communication with the WHIP server.
+       */
+      bearerToken?: string;
+      iceServers?: RTCConfiguration['iceServers'];
+      video: {
+        resolution: Api.Resolution;
+        maxBitrate?: number;
+      };
+      audio?: boolean;
     };
-    audio?: boolean;
-  };
 
 export function intoRegisterOutputRequest(request: RegisterOutput): Output.RegisterOutput {
   if (request.type === 'stream') {
