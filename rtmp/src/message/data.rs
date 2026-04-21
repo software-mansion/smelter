8193fdd1aebe105ac84d3ceaa9d369092e8706e8
Wojciech Kaźmierczak <wojciech.kazmierczak@swmansion.com>
2026-04-21 10:26:47 +0200

    allow command prefixed with @

--- rtmp/src/message/data.rs
+++ rtmp/src/message/data.rs
@@ -13,18 +13,24 @@ impl DataMessage {
         // onMetaData can appear as:
         //   ["@setDataFrame", "onMetaData", {properties}]
         //   ["onMetaData", {properties}]
+        // Some publishers additionally prefix the data message names with "@".
+        // Accept both forms.
         let mut iter = values.into_iter();
         match (iter.next(), iter.next(), iter.next()) {
             (
                 Some(AmfValue::String(s1)),
                 Some(AmfValue::String(s2)),
                 Some(AmfValue::Object(map) | AmfValue::EcmaArray(map)),
-            ) if s1 == "@setDataFrame" && s2 == "onMetaData" => Self::OnMetaData(map),
+            ) if matches!(s1.as_str(), "setDataFrame" | "@setDataFrame")
+                && matches!(s2.as_str(), "onMetaData" | "@onMetaData") =>
+            {
+                Self::OnMetaData(map)
+            }
             (
                 Some(AmfValue::String(s1)),
                 Some(AmfValue::Object(map) | AmfValue::EcmaArray(map)),
                 _,
-            ) if s1 == "onMetaData" => Self::OnMetaData(map),
+            ) if matches!(s1.as_str(), "onMetaData" | "@onMetaData") => Self::OnMetaData(map),
             (v1, v2, v3) => {
                 let first_3_values = [v1, v2, v3].into_iter().flatten();
                 Self::Unknown(first_3_values.chain(iter).collect())
