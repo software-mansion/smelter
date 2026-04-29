507e33a487dd2e5d58aad64fc09bee8c25cd3dfc
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2026-04-29 12:14:10 +0200

    [ts-sdk] Export missing types from smelter-core

--- ts/smelter-core/src/index.ts
+++ ts/smelter-core/src/index.ts
@@ -1,8 +1,15 @@
 import * as Output from './api/output';
 import * as Input from './api/input';
+import * as Image from './api/image';
 
-export { Output, Input };
-export { ApiClient, ApiRequest, MultipartRequest, RegisterInputResponse } from './api';
+export { Output, Input, Image };
+export {
+  ApiClient,
+  ApiRequest,
+  MultipartRequest,
+  RegisterInputResponse,
+  RegisterOutputResponse,
+} from './api';
 export { Smelter } from './live/compositor';
 export { OfflineSmelter } from './offline/compositor';
 export { SmelterManager, SetupInstanceOptions } from './smelterManager';
