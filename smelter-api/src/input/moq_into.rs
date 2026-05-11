be603750d8c7a73a91b7e5800282b1e8d0a9a2a8
Jakub Rękas <jakub.rekas@swmansion.com>
2026-05-11 10:46:15 +0200

    MoQ server Input

---
+++ smelter-api/src/input/moq_into.rs
@@ -0,0 +1,38 @@
+use crate::common_core::prelude as core;
+use crate::*;
+
+impl TryFrom<MoqInputServer> for core::RegisterInputOptions {
+    type Error = TypeError;
+
+    fn try_from(value: MoqInputServer) -> Result<Self, Self::Error> {
+        let MoqInputServer {
+            broadcast_path,
+            required,
+            decoder_map,
+            side_channel,
+        } = value;
+
+        let side_channel = side_channel.unwrap_or_default();
+
+        let h264 = decoder_map
+            .as_ref()
+            .and_then(|decoders| decoders.get(&InputMoqCodec::H264))
+            .map(|decoder| match decoder {
+                MoqVideoDecoderOptions::FfmpegH264 => Ok(core::VideoDecoderOptions::FfmpegH264),
+                MoqVideoDecoderOptions::VulkanH264 => Ok(core::VideoDecoderOptions::VulkanH264),
+            })
+            .transpose()?;
+
+        let input_options = core::MoqServerInputOptions {
+            broadcast_path,
+            decoders: core::MoqServerInputDecoders { h264, aac: None },
+            queue_options: core::QueueInputOptions {
+                required: required.unwrap_or(false),
+                video_side_channel: side_channel.video.unwrap_or(false),
+                audio_side_channel: side_channel.audio.unwrap_or(false),
+            },
+        };
+
+        Ok(core::RegisterInputOptions::MoqServer(input_options))
+    }
+}
