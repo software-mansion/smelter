7054709bf2387cebe1111ba4d91700210a2bcca5
Wojciech Kozyra <wojciech.kozyra@swmansion.com>
2026-05-27 14:43:47 +0200

    Remove demos (moved to separate repo) (#2029)

--- demos/vite-react-router-web-wasm/app/components/util.ts
+++
@@ -1,8 +0,0 @@
-export const getNewOutputId = (() => {
-  let counter = 1;
-  return () => {
-    const outputId = `output-${counter}`;
-    counter += 1;
-    return outputId;
-  };
-})();
--- demos/vite-react-router-web-wasm/app/routes.ts
+++
@@ -1,7 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home.tsx"),
-  route("stream", "routes/stream.tsx"),
-  route("canvas", "routes/canvas.tsx"),
-] satisfies RouteConfig;
