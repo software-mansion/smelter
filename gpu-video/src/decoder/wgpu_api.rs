0f2d9e82940dad425fecb9f511a8f7eb36287fb8
noituri <mikolaj.radkowski@protonmail.com>
2026-07-01 16:52:26 +0200

    wip

--- gpu-video/src/decoder/wgpu_api.rs
+++ gpu-video/src/decoder/wgpu_api.rs
@@ -1,49 +1,32 @@
+#[cfg(feature = "wgpu")]
 use crate::{
-    DecoderEvent, EncodedInputChunk, H264ParserError, OutputFrame, RawFrameData,
-    ReferenceManagementError, VideoBackendError,
-    frame_sorter::{DecodeResult, FrameSorter},
+    DecoderEvent, EncodedInputChunk, OutputFrame, VideoDecoderError,
+    decoder::VideoDecoderBackend,
+    frame_sorter::FrameSorter,
     parser::{
-        decoder_instructions::{DecoderInstruction, compile_to_decoder_instructions},
+        decoder_instructions::compile_to_decoder_instructions,
         h264::{AccessUnit, H264Parser},
         reference_manager::ReferenceContext,
     },
 };
 
+/// A decoder that outputs frames stored as [`wgpu::Texture`]s
 #[cfg(feature = "wgpu")]
-mod wgpu_api;
-#[cfg(feature = "wgpu")]
-pub use wgpu_api::*;
-
-// TODO: split to 2 traits
-pub(crate) trait VideoDecoderBackend {
-    fn decode_to_bytes(
-        &mut self,
-        decoder_instructions: &[DecoderInstruction],
-    ) -> Result<Vec<DecodeResult<RawFrameData>>, VideoDecoderError>;
-
-    #[cfg(feature = "wgpu")]
-    fn decode_to_wgpu_textures(
-        &mut self,
-        wgpu_device: &wgpu::Device,
-        decoder_instructions: &[DecoderInstruction],
-    ) -> Result<Vec<DecodeResult<wgpu::Texture>>, VideoDecoderError>;
-}
-
-/// A decoder that outputs frames stored as [`Vec<u8>`] with the raw pixel data.
-pub struct BytesDecoder {
+pub struct WgpuTexturesDecoder {
+    pub(crate) wgpu_device: wgpu::Device,
     pub(crate) decoder: Box<dyn VideoDecoderBackend>,
     pub(crate) parser: H264Parser,
     pub(crate) reference_ctx: ReferenceContext,
-    pub(crate) frame_sorter: FrameSorter<RawFrameData>,
+    pub(crate) frame_sorter: FrameSorter<wgpu::Texture>,
 }
 
-impl BytesDecoder {
-    /// The result is a sequence of frames. The payload of each [`OutputFrame`] struct is a [`Vec<u8>`]. Each [`Vec<u8>`] contains a single
-    /// decoded frame in the [NV12 format](https://en.wikipedia.org/wiki/YCbCr#4:2:0).
+#[cfg(feature = "wgpu")]
+impl WgpuTexturesDecoder {
+    /// The produced textures have the [`wgpu::TextureFormat::NV12`] format and can be used as a texture binding.
     pub fn decode(
         &mut self,
         frame: EncodedInputChunk<'_>,
-    ) -> Result<Vec<OutputFrame<RawFrameData>>, VideoDecoderError> {
+    ) -> Result<Vec<OutputFrame<wgpu::Texture>>, VideoDecoderError> {
         self.process_event(DecoderEvent::DecodeChunk(frame))
     }
 
@@ -51,7 +34,7 @@ impl BytesDecoder {
     ///
     /// Make sure that this is done when you have the knowledge that no more frames will be coming
     /// that need to be presented before the already decoded frames.
-    pub fn flush(&mut self) -> Result<Vec<OutputFrame<RawFrameData>>, VideoDecoderError> {
+    pub fn flush(&mut self) -> Result<Vec<OutputFrame<wgpu::Texture>>, VideoDecoderError> {
         self.process_event(DecoderEvent::Flush)
     }
 
@@ -61,7 +44,7 @@ impl BytesDecoder {
     pub fn process_event(
         &mut self,
         event: DecoderEvent<'_, AccessUnit>,
-    ) -> Result<Vec<OutputFrame<RawFrameData>>, VideoDecoderError> {
+    ) -> Result<Vec<OutputFrame<wgpu::Texture>>, VideoDecoderError> {
         match event {
             DecoderEvent::DecodeChunk(chunk) => {
                 let nalus = self.parser.parse(chunk.data, chunk.pts)?;
@@ -88,28 +71,12 @@ impl BytesDecoder {
     fn decode_access_units(
         &mut self,
         access_units: Vec<AccessUnit>,
-    ) -> Result<Vec<OutputFrame<RawFrameData>>, VideoDecoderError> {
+    ) -> Result<Vec<OutputFrame<wgpu::Texture>>, VideoDecoderError> {
         let instructions = compile_to_decoder_instructions(&mut self.reference_ctx, access_units)?;
-        let unsorted_frames = self.decoder.decode_to_bytes(&instructions)?;
+        let unsorted_frames = self
+            .decoder
+            .decode_to_wgpu_textures(&self.wgpu_device, &instructions)?;
         let sorted_frames = self.frame_sorter.put_frames(unsorted_frames);
         Ok(sorted_frames)
     }
-}
-
-#[derive(Debug, thiserror::Error)]
-pub enum VideoDecoderError {
-    #[error("H264 parser error: {0}")]
-    ParserError(#[from] H264ParserError),
-
-    #[error("Reference management error: {0}")]
-    ReferenceManagementError(#[from] ReferenceManagementError),
-
-    #[cfg(feature = "wgpu")]
-    #[error(
-        "VideoDevice was created without wgpu support. Initialize wgpu::Device using VideoAdapterExt::request_device_with_video_support"
-    )]
-    VideoDeviceWithoutWgpu,
-
-    #[error("Encoder error: {0}")]
-    BackendError(VideoBackendError),
 }
