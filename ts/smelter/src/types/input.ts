8d66c5b273d66f1cc9e119a2a5370ed8228830b6
Wojciech Kozyra <wkozyra95@gmail.com>
2026-08-31 14:19:37 +0200

    HLS default buffer

--- ts/smelter/src/types/input.ts
+++ ts/smelter/src/types/input.ts
@@ -125,7 +125,7 @@ export type RegisterHlsInput = {
   /**
    * Input buffer configuration.
    *
-   * Defaults: `desiredMs=4000`, `minMs=2000`, `maxMs=8000`.
+   * Defaults: `desiredMs=12000`, `minMs=2000`, `maxMs=24000`.
    */
   buffer?: InputBuffer | null;
 };
